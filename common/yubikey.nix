890fbddcbd75cfaf6609d0f7581969adda616b5a
Pablo Ovelleiro Corral <mail@pablo.tools>
2020-12-10 15:04:19 +0100

    fix yubikey

--- common/yubikey.nix
+++ common/yubikey.nix
@@ -7,6 +7,13 @@
     pinentryFlavor = "gtk2";
   };
 
+  # Needed for yubikey to work
+  environment.shellInit = ''
+    export GPG_TTY="$(tty)"
+    gpg-connect-agent /bye
+    export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+  '';
+
   # Setup Yubikey SSH and GPG
   services.pcscd.enable = true;
   services.udev.packages = [ pkgs.yubikey-personalization ];
