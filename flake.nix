1b3561cb5ccc4c592257d0d62bf1355eb04fa19f
Pablo Ovelleiro Corral <mail@pablo.tools>
2020-12-16 11:04:26 +0100

    put hardwarescan in config

--- flake.nix
+++ flake.nix
@@ -4,48 +4,32 @@
   outputs = { self, nixpkgs }: {
 
     # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
     # defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
     nixosConfigurations = {
 
       ahorn = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [
-          ./machines/ahorn/configuration.nix
-          ./machines/ahorn/hardware-configuration.nix
-        ];
+        modules = [ ./machines/ahorn/configuration.nix ];
       };
 
       birne = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [
-          ./machines/birne/configuration.nix
-          ./machines/birne/hardware-configuration.nix
-        ];
+        modules = [ ./machines/birne/configuration.nix ];
       };
 
       kartoffel = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [
-          ./machines/kartoffel/configuration.nix
-          ./machines/kartoffel/hardware-configuration.nix
-        ];
+        modules = [ ./machines/kartoffel/configuration.nix ];
       };
 
       kfbox = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [
-          ./machines/kfbox/configuration.nix
-          ./machines/kfbox/hardware-configuration.nix
-        ];
+        modules = [ ./machines/kfbox/configuration.nix ];
       };
 
       mega = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [
-          ./machines/mega/configuration.nix
-          ./machines/mega/hardware-configuration.nix
-        ];
+        modules = [ ./machines/mega/configuration.nix ];
       };
 
       porree = nixpkgs.lib.nixosSystem {
