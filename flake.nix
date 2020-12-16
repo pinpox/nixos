d79c0fbf8467b32d7c8da4781e3c40577413fbfd
Pablo Ovelleiro Corral <mail@pablo.tools>
2020-12-16 12:26:42 +0100

    remove hardware from flake.nix

--- flake.nix
+++ flake.nix
@@ -3,16 +3,12 @@
 
   outputs = { self, nixpkgs }: {
 
-    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    # defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
     nixosConfigurations = {
 
       ahorn = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./machines/ahorn/configuration.nix
-          ./machines/ahorn/hardware-configuration.nix
         ];
       };
 
@@ -20,7 +16,6 @@
         system = "x86_64-linux";
         modules = [
           ./machines/birne/configuration.nix
-          ./machines/birne/hardware-configuration.nix
         ];
       };
 
@@ -28,7 +23,6 @@
         system = "x86_64-linux";
         modules = [
           ./machines/kartoffel/configuration.nix
-          ./machines/kartoffel/hardware-configuration.nix
         ];
       };
 
@@ -36,7 +30,6 @@
         system = "x86_64-linux";
         modules = [
           ./machines/kfbox/configuration.nix
-          ./machines/kfbox/hardware-configuration.nix
         ];
       };
 
@@ -44,7 +37,6 @@
         system = "x86_64-linux";
         modules = [
           ./machines/mega/configuration.nix
-          ./machines/mega/hardware-configuration.nix
         ];
       };
 
