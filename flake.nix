1959cd7378009ff791bf3a59d9e1b6c4ee3bc208
Pablo Ovelleiro Corral <mail@pablo.tools>
2020-12-17 23:13:51 +0100

    put kartoffel modules in flake

--- flake.nix
+++ flake.nix
@@ -27,6 +27,26 @@
         system = "x86_64-linux";
         modules = [
           ./machines/kartoffel/configuration.nix
+
+    # Include the results of the hardware scan.
+    ./machines/kartoffel/hardware-configuration.nix
+
+    # User Profiles
+    ./common/user-profiles/pinpox.nix
+
+    # Include reusables
+    ./common/bluetooth.nix
+    ./common/environment.nix
+    ./common/fonts.nix
+    ./common/locale.nix
+    ./common/networking.nix
+    ./common/openssh.nix
+    ./common/sound.nix
+    ./common/virtualization.nix
+    ./common/xserver.nix
+    ./common/yubikey.nix
+    ./common/zsh.nix
+    #../../common/borg/home.nix
         ];
       };
 
