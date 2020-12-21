468fbd53859bb41387227c2d2ac0a160daf976ed
Pablo Ovelleiro Corral <mail@pablo.tools>
2020-12-21 12:16:58 +0100

    simplify mmonit module

--- modules/mmonit.nix
+++ modules/mmonit.nix
@@ -2,28 +2,21 @@
 
   nixpkgs.config.packageOverrides = pkgs: rec {
     mmonit = pkgs.callPackage ../packages/mmonit {};
-    mmonit-start = pkgs.writeScriptBin "mmonit-start" ''
-  #!${pkgs.stdenv.shell}
-
-
-FILE=/var/lib/mmonit/.created
-if [ ! -f "$FILE" ]; then
-  echo "$FILE not found, creating new mmonit home"
-  mkdir -p /var/lib/mmonit
-  cp -r ${mmonit}/* /var/lib/mmonit
-  chown -R mmonit:mmonit /var/lib/mmonit
-  chmod -R 600 /var/lib/mmonit
-  touch /var/lib/mmonit/.created
-fi
-    '';
-
-    mmonit-stop = pkgs.writeScriptBin "mmonit-start" ''
-  #!${pkgs.stdenv.shell}
-      exec ${mmonit}/bin/mmonit stop
+    mmonit-init = pkgs.writeScriptBin "mmonit-init" ''
+      #!${pkgs.stdenv.shell}
+      FILE=/var/lib/mmonit/.created
+      if [ ! -f "$FILE" ]; then
+        echo "$FILE not found, creating new mmonit home"
+        mkdir -p /var/lib/mmonit
+        cp -r ${mmonit}/* /var/lib/mmonit
+        chown -R mmonit:mmonit /var/lib/mmonit
+        chmod -R 600 /var/lib/mmonit
+        touch /var/lib/mmonit/.created
+      fi
     '';
   };
 
-  environment.systemPackages = with pkgs; [ mmonit mmonit-start mmonit-stop];
+  environment.systemPackages = with pkgs; [ mmonit mmonit-init ];
 
   users.users.mmonit = {
     isNormalUser = false;
@@ -36,35 +29,6 @@ fi
     name = "mmonit";
   };
 
-# Usage: mmonit [options] {arguments}
-# Options are as follows:
-#   -c file   Specify an alternate server.xml control file
-#   -r dir    Specify an alternate m/monit home directory
-#   -p dir    Specify a directory for the mmonit.pid lock file
-#   -w dir    Specify an alternate temporary working directory. Default is /tmp
-#   -i        Do not start mmonit in daemon mode. (Run in foreground)
-#   -t        Run syntax check for configuration files
-#   -l        Print license information
-#   -v        Print version number
-#   -d        Diagnostic mode, work noisy (dump core on error)
-#   -h        Print this text
-# Arguments are as follows:
-#   start     Start mmonit (default)
-#   stop      Stop mmonit
-
-# from AUR
-# [Unit]
-# Description = Easy, proactive monitoring of Unix systems, network and cloud services
-# After = network.target
-
-# [Service]
-# Type=forking
-# ExecStart = /opt/mmonit/bin/mmonit start
-# ExecStop = /opt/mmonit/bin/mmonit stop
-# PIDFile = /opt/mmonit/logs/mmonit.pid
-
-# [Install]
-# WantedBy = multi-user.target
 
   networking.firewall = {
     enable = true;
@@ -76,11 +40,9 @@ systemd.services.mmonit = {
   description = "M/monit Monitoring";
   serviceConfig = {
      # Type = "forking";
-     # ExecStart = "mmonit-start";
-     # PermissionsStartOnly=true
-     preStart = "+/run/current-system/sw/bin/mmonit-start";
+     preStart = "+/run/current-system/sw/bin/mmonit-init";
      ExecStart = "${pkgs.mmonit}/bin/mmonit -r /var/lib/mmonit -i start";
-     # ExecStop = "mmonit-stop";
+     ExecStop = "${pkgs.mmonit}/bin/mmonit stop";
      Restart = "on-failure";
    };
    wantedBy = [ "default.target" ];
