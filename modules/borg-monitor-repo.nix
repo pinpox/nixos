8eec499269208defe7a981622640e7e266e7d900
Pablo Ovelleiro Corral <mail@pablo.tools>
2020-12-28 14:27:49 +0100

    use pathShebangs for borg monitor script wrapper

--- modules/borg-monitor-repo.nix
+++ modules/borg-monitor-repo.nix
@@ -1,13 +1,15 @@
 { pkgs, ... }:
 
 let
-  scriptDeps = [ pkgs.jq pkgs.borgbackup pkgs.bash ];
+  scriptDeps = [ pkgs.jq pkgs.borgbackup ];
 
   borg-monitor-repo = pkgs.runCommandLocal "borg-monitor-repo" {
     nativeBuildInputs = [ pkgs.makeWrapper ];
   } ''
     mkdir -p "$out/bin"
-    makeWrapper "${./borg-check.sh}" "$out/bin/borg-monitor-repo" \
+    cp "${./borg-check.sh}" "$out/bin/borg-monitor-repo"
+    patchShebangs "$out/bin/borg-monitor-repo"
+    wrapProgram "$out/bin/borg-monitor-repo" \
       --prefix PATH : "${pkgs.lib.makeBinPath scriptDeps}"
   '';
 
