10edc9d3ad7d12167be52757e947d84af5a4bf95
Pablo Ovelleiro Corral <github@pablo.tools>
2021-01-10 23:14:41 +0100

    check if we can set NIXPATH

--- flake.nix
+++ flake.nix
@@ -3,9 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     nixos-home.url = "github:pinpox/nixos-home";
+    nixos-home.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs, home-manager, nixos-home }:
     let
@@ -49,6 +52,14 @@
             {
               home-manager.users.pinpox = nixos-home.nixosModules.desktop;
             }
+
+            {
+ nix.nixPath = [
+    "nixpkgs=${nixpkgs}"
+    "nixos-config=/etc/nixos/configuration.nix"
+    "nixpkgs-overlays=/etc/nixos/overlays"
+  ];
+}
 
             # Modules
             ./modules/bluetooth.nix
