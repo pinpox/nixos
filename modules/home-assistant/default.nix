f49ba611b8cb488384de85aa2ab97ce00aa9bcf1
Pablo Ovelleiro Corral <github@pablo.tools>
2021-01-22 19:42:51 +0100

    add some config

--- modules/home-assistant/default.nix
+++ modules/home-assistant/default.nix
@@ -1,27 +1,34 @@
 { config, pkgs, ... }: {
 
+  # Needed for some integrations
+  users.users.hass.extraGroups = [ "dialout" ];
+
   # Enable home-assistant
   services.home-assistant = {
     enable = true;
 
+  # Disable the python checks, they take for ever
+  package =
+    (pkgs.home-assistant.overrideAttrs (old: { doCheck = false; }));
+
     config = {
-    default_config = {};
+      # default_config = {};
       # Basic settings
       homeassistant = {
         name = "Home";
         latitude = "32.8753367";
         longitude = "-117.2474053";
-        elevation = 22;
+        elevation = 86;
         unit_system = "metric";
-        time_zone = "Europe/Stockholm";
+        time_zone = "Europe/Berlin";
         external_url = "https://home.pablo.tools";
       };
 
-      # # Discover some devices automatically
-      # discovery = { };
+      # Discover some devices automatically
+      discovery = { };
 
-      # # Show some system health data
-      # system_health = { };
+      # Show some system health data
+      system_health = { };
 
       # Http settings
       http = {
@@ -30,9 +37,39 @@
         trusted_proxies = "127.0.0.1";
         server_port = 8123;
       };
+      mqtt = {
+
+        broker = "127.0.0.1";
+        # certificate = "auto";
+        # port = "8883";
+        # username = "hass@thalheim.io";
+        # password = "!secret ldap_password";
+      };
+
+      # Enables a map showing the location of tracked devies
+      map = { };
 
       # Track the sun
-      # sun = { };
+      sun = { };
+
+      # Enable mobile app
+      mobile_app = { };
+
+      # Enable configuration UI
+      config = { };
+      # # Make the ui configurable through ui-lovelace.yaml
+      #   lovelace.mode = "yaml";
+      #   lovelace.resources = [
+      #     { url = "/local/vacuum-card.js";  type = "module"; }
+      #   ];
+      # Enable support for tracking state changes over time
+      history = { };
+
+      # Purge tracked history after 10 days
+      recorder.purge_keep_days = 10;
+
+      # View all events in o logbook
+      logbook = { };
 
     };
   };
