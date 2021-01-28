a5d9579952232ea47a9297083a593722ffa9417c
Pablo Ovelleiro Corral <github@pablo.tools>
2021-01-28 20:04:08 +0100

    workaround for netcup image

--- images/qemu/build.sh
+++ images/qemu/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-nix-build '<nixpkgs/nixos>' -A config.system.build.qcow2 --arg configuration "{ imports = [ ./build-qcow2.nix ]; }"
+GC_DONT_GC=1 nix-build -v '<nixpkgs/nixos>' -A config.system.build.qcow2 --arg configuration "{ imports = [ ./build-qcow2.nix ]; }"
