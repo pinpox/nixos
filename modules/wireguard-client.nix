62d4a7cdd3ec69a180bab70b722bf1325ccbd11f
Pablo Ovelleiro Corral <github@pablo.tools>
2021-03-07 23:23:08 +0100

    rewrite wg module to accept ip

--- modules/wireguard-client.nix
+++ modules/wireguard-client.nix
@@ -1,24 +1,41 @@
-{ config, pkgs, lib, ... }: {
-  networking.wireguard.interfaces = {
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.pinpox.wg-client;
+in {
 
-    wg0 = {
+  options.pinpox.wg-client = {
+    enable = mkEnableOption "hello service";
+    clientIp = mkOption {
+      type = types.str;
+      default = "0.0.0.0";
+    };
+  };
 
-      # Path to the private key file
-      privateKeyFile = "/var/src/secrets/wireguard/private";
-      peers = [{
-        # Public key of the server (not a file path).
-        publicKey = "XKqEk5Hsp3SRVPrhWD2eLFTVEYb9NYRky6AermPG8hU=";
+  config = mkIf cfg.enable {
 
-        # Don't forward all the traffic via VPN, only particular subnets
-        allowedIPs = [ "192.168.7.0/24" ];
+    networking.wireguard.interfaces = {
 
-        # Server IP and port.
-        endpoint = "vpn.pablo.tools:51820";
+      wg0 = {
 
-        # Send keepalives every 25 seconds. Important to keep NAT tables
-        # alive.
-        persistentKeepalive = 25;
-      }];
+        ips = [ cfg.clientIp ];
+
+        # Path to the private key file
+        privateKeyFile = "/var/src/secrets/wireguard/private";
+        peers = [{
+          # Public key of the server (not a file path).
+          publicKey = "XKqEk5Hsp3SRVPrhWD2eLFTVEYb9NYRky6AermPG8hU=";
+
+          # Don't forward all the traffic via VPN, only particular subnets
+          allowedIPs = [ "192.168.7.0/24" ];
+
+          # Server IP and port.
+          endpoint = "vpn.pablo.tools:51820";
+
+          # Send keepalives every 25 seconds. Important to keep NAT tables
+          # alive.
+          persistentKeepalive = 25;
+        }];
+      };
     };
   };
 }
