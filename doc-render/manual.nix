776bbe8e8e67afb67428dc2b3c50d9a4e694d58c
Pablo Ovelleiro Corral <github@pablo.tools>
2021-03-10 12:42:54 +0100

    better json output

--- doc-render/manual.nix
+++ doc-render/manual.nix
@@ -1,7 +1,8 @@
 with import <nixpkgs> { };
 let
-  eval = import (pkgs.path + "/nixos/lib/eval-config.nix") { modules = [ ./hello.nix]; };
+  eval = import (pkgs.path + "/nixos/lib/eval-config.nix") { modules = [ ./hello.nix ../modules/wireguard-client.nix]; };
   opts = (nixosOptionsDoc { options = eval.options; }).optionsJSON;
 in runCommandLocal "options.json" { inherit opts; }
-  "cat $opts/share/doc/nixos/options.json | ${pkgs.jq}/bin/jq 'to_entries | .[] | select(.key|test(\"pinpox\"))' > $out"
+"cat $opts/share/doc/nixos/options.json | ${pkgs.jq}/bin/jq '.| with_entries( select(.key|contains(\"pinpox\") ) )' > $out"
+
 
