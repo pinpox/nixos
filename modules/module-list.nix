b276e3f2749a6d4acde4b6f2f5c6da592fce16ac
Pablo Ovelleiro Corral <github@pablo.tools>
2021-03-12 00:41:43 +0100

    add all modules to list

--- modules/module-list.nix
+++ modules/module-list.nix
@@ -3,5 +3,46 @@
   # All modules added here should have an 'enable' option
   ./wireguard-client.nix
   ./environment.nix
+  ./zsh.nix
   ./hello.nix # Example module
+# ./base/desktop.nix
+./bepasty
+./bluetooth.nix
+./borg
+./borg-monitor-repo.nix
+./borg-server.nix
+./chromium.nix
+./custom-packages.nix
+./drone-ci
+./dyndns.nix
+./environment.nix
+./fonts.nix
+./hedgedoc.nix
+./hello.nix
+./home-assistant
+./locale.nix
+./lvm-grub.nix
+./mattermost
+./mmonit.nix
+./monit
+./monitoring/loki.nix
+./monitoring/telegraf.nix
+./monitoring/prometheus.nix
+./munin-master.nix
+./networking.nix
+./nix-common.nix
+./openssh.nix
+./sound.nix
+./thelounge.nix
+# ./user-profiles/root.nix
+# ./user-profiles/pinpox.nix
+./virtualization.nix
+./wireguard-client.nix
+./xserver.nix
+./yubikey.nix
+./zabbix-agent.nix
+./zabbix-server.nix
+./zsh.nix
 ]
+
+
