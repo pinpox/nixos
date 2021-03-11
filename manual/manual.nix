a052bba3b2b7a9a480f175e4687b17fa472415cf
Pablo Ovelleiro Corral <github@pablo.tools>
2021-03-11 19:28:53 +0100

    static renderer

--- manual/manual.nix
+++ manual/manual.nix
@@ -1,8 +1,47 @@
 with import <nixpkgs> { };
 let
-  eval = import (pkgs.path + "/nixos/lib/eval-config.nix") { modules = [ ./hello.nix ../modules/wireguard-client.nix]; };
+  eval = import (pkgs.path + "/nixos/lib/eval-config.nix") {
+    modules = [ ./hello.nix ../modules/wireguard-client.nix ];
+  };
   opts = (nixosOptionsDoc { options = eval.options; }).optionsJSON;
-in runCommandLocal "options.json" { inherit opts; }
-"cat $opts/share/doc/nixos/options.json | ${pkgs.jq}/bin/jq '.| with_entries( select(.key|contains(\"pinpox\") ) )' > $out"
+
+  templateMarkdown = pkgs.writeTextFile {
+    name = "markdown";
+    text = builtins.readFile ./templates/markdown.mustache;
+  };
 
 
+  templateHTML = pkgs.writeTextFile {
+    name = "html";
+    text = builtins.readFile ./templates/html.mustache;
+  };
+
+in rec {
+
+
+
+  json = runCommandLocal "options.json" { inherit opts; } ''
+    cat $opts/share/doc/nixos/options.json | \
+    ${pkgs.jq}/bin/jq '.| with_entries( select(.key|contains("pinpox") ) )' \
+    > $out
+  '';
+
+  markdown = runCommandLocal "options.md" { inherit opts; } ''
+    cat $opts/share/doc/nixos/options.json | \
+    ${pkgs.jq}/bin/jq '.| with_entries( select(.key|contains("pinpox") ) ) | [to_entries[]] | {options: .}' | \
+    ${pkgs.mustache-go}/bin/mustache ${templateMarkdown} \
+    > $out
+    '';
+
+  html = runCommandLocal "options.md" { inherit opts; } ''
+    cat $opts/share/doc/nixos/options.json | \
+    ${pkgs.jq}/bin/jq '.| with_entries( select(.key|contains("pinpox") ) ) | [to_entries[]] | {options: .}' | \
+    ${pkgs.mustache-go}/bin/mustache ${templateHTML} \
+    > $out
+    '';
+
+}
+
+
+
+# cat data.json| jq '[to_entries[]] | {options: .}' | ~/.go/bin/mustache html.mustache
