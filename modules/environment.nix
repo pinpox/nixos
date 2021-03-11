dd8c2d01be22690b49d86a41f24d2deed6b1feb1
Pablo Ovelleiro Corral <github@pablo.tools>
2021-03-12 00:31:09 +0100

    add enable option to environment.nix

--- modules/environment.nix
+++ modules/environment.nix
@@ -1,14 +1,24 @@
-{ config, pkgs, lib, ... }: {
-  # System-wide environment variables to be set
-  environment = {
-    variables = {
-      EDITOR = "nvim";
-      GOPATH = "~/.go";
-      VISUAL = "nvim";
-      # Use librsvg's gdk-pixbuf loader cache file as it enables gdk-pixbuf to load
-      # SVG files (important for icons)
-      GDK_PIXBUF_MODULE_FILE =
-        "$(echo ${pkgs.librsvg.out}/lib/gdk-pixbuf-2.0/*/loaders.cache)";
+{ config, pkgs, lib, ... }:
+let cfg = config.defaults.environment;
+in {
+
+  options.pinpox.defaults.environment = {
+    enable = mkEnableOption "Environment defaults";
+  };
+
+  config = mkIf cfg.enable {
+
+    # System-wide environment variables to be set
+    environment = {
+      variables = {
+        EDITOR = "nvim";
+        GOPATH = "~/.go";
+        VISUAL = "nvim";
+        # Use librsvg's gdk-pixbuf loader cache file as it enables gdk-pixbuf to load
+        # SVG files (important for icons)
+        GDK_PIXBUF_MODULE_FILE =
+          "$(echo ${pkgs.librsvg.out}/lib/gdk-pixbuf-2.0/*/loaders.cache)";
+      };
     };
   };
 }
--- modules/module-list.nix
+++ modules/module-list.nix
@@ -2,5 +2,6 @@
 
   # All modules added here should have an 'enable' option
   ./wireguard-client.nix
+  ./environment.nix
   ./hello.nix # Example module
 ]
