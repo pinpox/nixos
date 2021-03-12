22360ca473098cdb83c9134f971f887d3f03e366
Pablo Ovelleiro Corral <github@pablo.tools>
2021-03-12 08:18:36 +0100

    nixfmt module list

--- modules/module-list.nix
+++ modules/module-list.nix
@@ -1,48 +1,41 @@
 [
-
-  # All modules added here should have an 'enable' option
-  ./wireguard-client.nix
+  # ./base/desktop.nix
+  # ./user-profiles/pinpox.nix
+  # ./user-profiles/root.nix
+  ./bepasty
+  ./bluetooth.nix
+  ./borg
+  ./borg-monitor-repo.nix
+  ./borg-server.nix
+  ./chromium.nix
+  ./custom-packages.nix
+  ./drone-ci
+  ./dyndns.nix
   ./environment.nix
-  ./zsh.nix
+  ./fonts.nix
+  ./hedgedoc.nix
+  ./hello.nix
   ./hello.nix # Example module
-# ./base/desktop.nix
-./bepasty
-./bluetooth.nix
-./borg
-./borg-monitor-repo.nix
-./borg-server.nix
-./chromium.nix
-./custom-packages.nix
-./drone-ci
-./dyndns.nix
-./environment.nix
-./fonts.nix
-./hedgedoc.nix
-./hello.nix
-./home-assistant
-./locale.nix
-./lvm-grub.nix
-./mattermost
-./mmonit.nix
-./monit
-./monitoring/loki.nix
-./monitoring/telegraf.nix
-./monitoring/prometheus.nix
-./munin-master.nix
-./networking.nix
-./nix-common.nix
-./openssh.nix
-./sound.nix
-./thelounge.nix
-# ./user-profiles/root.nix
-# ./user-profiles/pinpox.nix
-./virtualization.nix
-./wireguard-client.nix
-./xserver.nix
-./yubikey.nix
-./zabbix-agent.nix
-./zabbix-server.nix
-./zsh.nix
+  ./home-assistant
+  ./locale.nix
+  ./lvm-grub.nix
+  ./mattermost
+  ./mmonit.nix
+  ./monit
+  ./monitoring/loki.nix
+  ./monitoring/prometheus.nix
+  ./monitoring/telegraf.nix
+  ./munin-master.nix
+  ./networking.nix
+  ./nix-common.nix
+  ./openssh.nix
+  ./sound.nix
+  ./thelounge.nix
+  ./virtualization.nix
+  ./wireguard-client.nix
+  ./xserver.nix
+  ./yubikey.nix
+  ./zabbix-agent.nix
+  ./zabbix-server.nix
+  ./zsh.nix
 ]
-
-
