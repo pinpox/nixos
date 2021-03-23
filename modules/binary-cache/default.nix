ad51b16e358a3044ffbd0b5627127de61ffae41d
Pablo Ovelleiro Corral <github@pablo.tools>
2021-03-24 00:32:22 +0100

    Fix cache user
    - add user to allowedUsers
    - fix reverse proxy
    - fix path to secret key

--- modules/binary-cache/default.nix
+++ modules/binary-cache/default.nix
@@ -5,8 +5,8 @@ let
   init-script = pkgs.writeScriptBin "write-key" ''
     #!${pkgs.stdenv.shell}
     cat /var/src/secrets/binary-cache/cache-priv-key.pem > /var/lib/cache-priv-key.pem
-    chown nix-serve /var/cache-priv-key.pem
-    chmod 600 /var/cache-priv-key.pem
+    chown nix-serve /var/lib/cache-priv-key.pem
+    chmod 600 /var/lib/cache-priv-key.pem
   '';
 in {
 
@@ -17,18 +17,25 @@ in {
   config = mkIf cfg.enable {
     services.nix-serve = {
       enable = true;
-      secretKeyFile = "/var/cache-priv-key.pem";
+      secretKeyFile = "/var/lib/cache-priv-key.pem";
     };
 
+    # TODO fix this, currently broken preStart is not a valid key
     systemd.services.nix-serve = {
       serviceConfig = { preStart = "+${init-script}/bin/write-key"; };
     };
 
+
+    nix.allowedUsers = [ "nix-serve" ];
+
     services.nginx = {
       enable = true;
       virtualHosts = {
+
         "cache.lounge.rocks" = {
-          serverAliases = [ "binarycache" ];
+      addSSL = true;
+      enableACME = true;
+          # serverAliases = [ "cache" ];
           locations."/".extraConfig = ''
             proxy_pass http://localhost:${
               toString config.services.nix-serve.port
