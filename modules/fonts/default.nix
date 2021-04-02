47c1ab143225af534ac9dbb9d5e36df719fc716b
Pablo Ovelleiro Corral <github@pablo.tools>
2021-04-02 18:22:07 +0200

    Install fonts system-wide

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -1,50 +1,30 @@
 { config, pkgs, lib, ... }:
 with lib;
 let cfg = config.pinpox.defaults.fonts;
-# custominput = pkgs.callPackage ./input.nix {};
-monego = pkgs.callPackage ./monego.nix {};
 in {
 
   options.pinpox.defaults.fonts = { enable = mkEnableOption "Fonts defaults"; };
 
   config = mkIf cfg.enable {
+
     # Install some fonts system-wide, especially "Source Code Pro" in the
     # Nerd-Fonts pached version with extra glyphs.
+
     fonts = {
       fontDir.enable = true;
       fonts = with pkgs; [
-        # TODO Try
-        # Roboto Mono
-        # Ubuntu mono
-        # Hack
-        # Victor Mono
-        # custominput
-        monego
-
-        (nerdfonts.override {
-          fonts = [
-            "SourceCodePro"
-            "Ubuntu"
-            "UbuntuMono"
-            "VictorMono"
-            "RobotoMono"
-            "Hermit"
-            "Hack"
-            "FiraMono"
-            "Go-Mono"
-          ];
-        })
-        ubuntu_font_family
-        pkgs.dejavu_fonts
+        (nerdfonts.override { fonts = [ "SourceCodePro" ]; })
+        source-sans-pro
+        source-serif-pro
         noto-fonts-emoji
         corefonts
       ];
 
       fontconfig = {
         defaultFonts = {
-          serif = [ "Ubuntu" ];
-          sansSerif = [ "Ubuntu" ];
-          monospace = [ "Ubuntu" ];
+          serif = [ "Source Serif Pro" ];
+          sansSerif = [ "Source Sans Pro" ];
+          monospace = [ "SauceCodePro Nerd Font Mono" ];
         };
       };
     };
