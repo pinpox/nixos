e75302d026e227ecc370691b870b46b0d65a05b1
Pablo Ovelleiro Corral <github@pablo.tools>
2021-04-05 23:28:54 +0200

    Reduce krops command output

--- flake.nix
+++ flake.nix
@@ -13,6 +13,9 @@
 
     nixos-home.url = "github:pinpox/nixos-home";
     nixos-home.inputs.nixpkgs.follows = "nixpkgs";
+
+    mayniklas.url = "github:mayniklas/nixos";
+    mayniklas.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, ... }@inputs:
     with inputs;
@@ -26,6 +29,7 @@
 
             ({ ... }: {
               imports = builtins.attrValues self.nixosModules ++ [
+                mayniklas.nixosModules.hellonik
                 {
                   # Set the $NIX_PATH entry for nixpkgs. This is necessary in
                   # this setup with flakes, otherwise commands like `nix-shell
