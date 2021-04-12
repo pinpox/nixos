0e764716ccd3bb006f7c456022d0f42bddc4a8de
Pablo Ovelleiro Corral <github@pablo.tools>
2021-04-13 01:25:55 +0200

    Fix wezterm-bin package

--- packages/wezterm-bin/default.nix
+++ packages/wezterm-bin/default.nix
@@ -1,20 +1,65 @@
-{ stdenv, fetchurl }:
+{ fetchurl, pkgs, dbus, egl-wayland, fetchFromGitHub, fontconfig, freetype, lib
+, libGL, libGLU, libX11, libglvnd # libEGL.so.1
+, libiconv, libxcb, libxkbcommon, openssl, perl, pkg-config, python3
+, rustPlatform, stdenv, wayland, xcbutil, xcbutilimage, xcbutilkeysyms
+, xcbutilwm # contains xcb-ewmh among others
+, zlib
 
-stdenv.mkDerivation rec {
+}:
+
+let
+  rpath = stdenv.lib.makeLibraryPath [
+    dbus
+    egl-wayland
+    fontconfig
+    fontconfig.lib
+    freetype
+    libGL
+    libGLU
+    libglvnd
+    libxkbcommon
+    openssl
+    wayland
+    libX11
+    libxcb
+    xcbutil
+    xcbutilimage
+    xcbutilkeysyms
+    xcbutilwm
+    zlib
+
+  ];
+
+in stdenv.mkDerivation rec {
   pname = "wezterm-bin";
   version = "nightly-2021";
 
   src = fetchurl {
-    url = "https://github.com/wez/wezterm/releases/download/nightly/wezterm-nightly.Ubuntu16.04.tar.xz";
-    sha256 = "0ssi1wpaf7plaswqqjwigppsg5fyh99vdlb9kzl7c9lng89ndq1i";
+    url =
+      "https://github.com/wez/wezterm/releases/download/nightly/wezterm-nightly.Ubuntu16.04.tar.xz";
+    sha256 = "sha256-b6J/DuALK1WN4EPFgzLDZh1gRvAkwMWTIIaxx+KsoHE=";
   };
 
+  nativeBuildInputs = [ pkg-config python3 perl ];
 
-  doCheck = true;
+  # prevent further changes to the RPATH
+  dontPatchELF = true;
+
+  installPhase = ''
+     mkdir -p $out
+    cp -r ./usr/* $out/
+  '';
+
+  postFixup = ''
+       for artifact in wezterm wezterm-gui wezterm-mux-server strip-ansi-escapes; do
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$out/bin/$artifact" || true
+         patchelf --set-rpath "${rpath}" $out/bin/$artifact
+       done
+     '';
 
   meta = with stdenv.lib; {
     description = "";
-    longDescription = '' '';
+    longDescription = "";
     homepage = "";
     changelog = "";
     license = licenses.gpl3Plus;
