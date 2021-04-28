fa16299615a82fabaac0da3a7813d02139476a13
Pablo Ovelleiro Corral <github@pablo.tools>
2021-04-28 08:28:46 +0200

    Sort general vim options

--- home-manager/modules/vim/lua/init.lua
+++ home-manager/modules/vim/lua/init.lua
@@ -2,42 +2,43 @@
 -- vim.bo: buffer-local options
 -- vim.wo: window-local options
 
-vim.o.completeopt      = "menuone,noselect"
-vim.o.hidden           = true            -- Buffer becomes hidden when it is abandoned but is not unloaded
-vim.o.title            = true                   -- Show title in terminal window
+-- vim.o.path            += "**"              -- Search down into subfolders, provides tab-completion for all file-related tasks
 vim.o.autochdir        = true               -- Automatically change the working dir to the one of the file
-vim.o.autoindent       = true              -- always set autoindenting on
+vim.o.autoindent       = true               -- always set autoindenting on
 vim.o.backspace        = "indent,eol,start" -- allow backspacing over everything in insert mode
+vim.o.backup           = false              -- dont create backups
 vim.o.clipboard        = "unnamedplus"      -- Set Clipboard to system's clipboard
-vim.o.cursorline       = true                 -- highlight current line
+vim.o.completeopt      = "menuone,noselect"
+vim.o.cursorline       = true               -- highlight current line
 vim.o.directory        = "~/.vimtmp"        -- Dir for temp files
-vim.o.foldlevelstart   = 20          -- start with open folds
-vim.o.foldmethod       = "syntax"          -- set folding based on syntax
+vim.o.expandtab        = false              -- Don't expand tabs to spaces
+vim.o.foldlevelstart   = 20                 -- start with open folds
+vim.o.foldmethod       = "syntax"           -- set folding based on syntax
+vim.o.hidden           = true               -- Buffer becomes hidden when it is abandoned but is not unloaded
 vim.o.history          = 50                 -- keep 50 lines of command line history
-vim.o.hlsearch         = true                   -- highlight matches
-vim.o.ignorecase       = true                 -- Case insensitive search
-vim.o.incsearch        = true                  -- search as characters are entered
-vim.o.laststatus       = 2               -- Always show the status line
-vim.o.lazyredraw       = true                 -- redraw only when we need to.
-vim.wo.relativenumber  = true             -- show relative line numbers
-vim.wo.number          = true                     -- show absolute line numbers
-vim.o.ruler            = true                      -- show the cursor position all the time
-vim.o.scrolloff        = 5                -- show lines above and below when scrolling
-vim.o.showcmd          = true                    -- display incomplete commands
-vim.o.smartcase        = true                  -- Case sensitive then capital is typed
-vim.o.synmaxcol        = 200              -- Maximum length of syntax highlighting
-vim.o.tabstop          = 4                  -- A tab is displayed 4 collumns wide
-vim.o.softtabstop      = 4              -- Size of a tab
-vim.o.shiftwidth       = 4               -- Size of a tab
-vim.o.expandtab        = false -- Don't expand tabs to spaces
-vim.o.undofile         = true                   -- Maintain undo history between sessions
-vim.o.undodir          = "~/.vimtmp"          -- Dir for undofiles, same dir as the tempdir
-vim.o.wildmenu         = true                   -- Complete commands
-vim.o.wrapscan         = true                   -- Wrap when searching to beginning
+vim.o.hlsearch         = true               -- highlight matches
+vim.o.ignorecase       = true               -- Case insensitive search
+vim.o.incsearch        = true               -- search as characters are entered
+vim.o.laststatus       = 2                  -- Always show the status line
+vim.o.lazyredraw       = true               -- redraw only when we need to.
 vim.o.mouse            = "a"
-vim.o.swapfile         = false                 -- dont create a swapfile
-vim.o.backup           = false-- dont create backups
--- vim.o.path            += "**"                   -- Search down into subfolders, provides tab-completion for all file-related tasks
+vim.o.ruler            = true               -- show the cursor position all the time
+vim.o.scrolloff        = 5                  -- show lines above and below when scrolling
+vim.o.shiftwidth       = 4                  -- Size of a tab
+vim.o.showcmd          = true               -- display incomplete commands
+vim.o.smartcase        = true               -- Case sensitive then capital is typed
+vim.o.softtabstop      = 4                  -- Size of a tab
+vim.o.swapfile         = false              -- dont create a swapfile
+vim.o.synmaxcol        = 200                -- Maximum length of syntax highlighting
+vim.o.tabstop          = 4                  -- A tab is displayed 4 collumns wide
+vim.o.title            = true               -- Show title in terminal window
+vim.o.undodir          = "~/.vimtmp"        -- Dir for undofiles, same dir as the tempdir
+vim.o.undofile         = true               -- Maintain undo history between sessions
+vim.o.wildmenu         = true               -- Complete commands
+vim.o.wrapscan         = true               -- Wrap when searching to beginning
+
+vim.wo.number          = true               -- show absolute line numbers
+vim.wo.relativenumber  = true               -- show relative line numbers
 
 
 require'compe'.setup {
