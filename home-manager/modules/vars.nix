78180b0cabe48a23c206c32aaedf945c9dd55270
Pablo Ovelleiro Corral <github@pablo.tools>
2021-05-02 20:06:50 +0200

    Finish all color definitions

--- home-manager/modules/vars.nix
+++ home-manager/modules/vars.nix
@@ -1,40 +1,39 @@
 {
   colors = rec {
 
-    Black = "2E3440"; # #2E3440
+    Black = "2E3440";         #2E3440
 
-    DarkGrey = "3B4252"; # #3B4252
-    Grey = "434C5E"; # #434C5E
-    BrightGrey = "4C566A"; # #4C566A
+    DarkGrey = "3B4252";      #3B4252
+    Grey = "434C5E";          #434C5E
+    BrightGrey = "4C566A";    #4C566A
 
-    DarkWhite = "D8DEE9"; # #D8DEE9
-    White = "E5E9F0"; # #E5E9F0
-    BrightWhite = "ECEFF4"; # #ECEFF4
+    DarkWhite = "D8DEE9";     #D8DEE9
+    White = "E5E9F0";         #E5E9F0
+    BrightWhite = "ECEFF4";   #ECEFF4
 
-    DarkYellow = "F78C6C"; # #F78C6C
-    Yellow = "FFCB6B"; # #FFCB6B
-    BrightYellow = ""; # TODO
+    DarkYellow = "F78C6C";    #F78C6C
+    Yellow = "FFCB6B";        #FFCB6B
+    BrightYellow = "FFE66B";  #ffe66b
 
-    DarkGreen = "55D1B4"; # #55D1B4 Previously: #8FBCBB
-    Green = "C3E88D"; # #C3E88D
-    BrightGreen = ""; # TODO
+    DarkGreen = "55D1B4";     #55D1B4
+    Green = "C3E88D";         #C3E88D
+    BrightGreen = "68f288";   #68f288
 
-    DarkCyan = ""; # TODO
-    Cyan = "89DDFF"; # #89DDFF
-    BrightCyan = ""; # TODO
+    DarkCyan = "6EB1CC";      #6EB1CC
+    Cyan = "89DDFF";          #89DDFF
+    BrightCyan = "A1E4FF";    #A1E4FF
 
-    DarkBlue = ""; # #TODO
-    Blue = "82AAFF"; # #82AAFF
-    BrightBlue = ""; # #TODO
+    DarkBlue = "5B77B3";      #5B77B3
+    Blue = "82AAFF";          #82AAFF
+    BrightBlue = "9BBBFF";    #9BBBFF
 
-    DarkMagenta = ""; # #TODO
-    Magenta = "C792EA"; # #C792EA
-    BrightMagenta = ""; # #TODO
+    DarkMagenta = "8B66A4";   #8B66A4
+    Magenta = "C792EA";       #C792EA
+    BrightMagenta = "D8B3F0"; #D8B3F0
 
-    BrightRed = "FF5370"; # #FF5370
-    Red = "F07178"; # #F07178
-    DarkRed = ""; # TODO
-
+    DarkRed = "A61C23";       #A61C23
+    Red = "F07178";           #F07178
+    BrightRed = "FF5370";     #FF5370
   };
 
   font = {
