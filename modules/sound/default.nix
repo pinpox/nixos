5a3d18a85c72f4f7cee6328169ad554d6c791c75
Pablo Ovelleiro Corral <github@pablo.tools>
2021-06-07 17:35:33 +0200

    Connect bluetooth devices when powering on

--- modules/sound/default.nix
+++ modules/sound/default.nix
@@ -12,6 +12,9 @@ in {
       enable = true;
       package = pkgs.pulseaudioFull;
       extraModules = [ pkgs.pulseaudio-modules-bt ];
+      extraConfig = ''
+        load-module module-switch-on-connect
+      '';
     };
   };
 }
