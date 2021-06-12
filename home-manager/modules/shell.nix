de40114a345906f59d6d9c73e9c66adbe83d0824
Pablo Ovelleiro Corral <github@pablo.tools>
2021-06-12 14:44:22 +0200

    Set ZK_NOTEBOOK_DIR

--- home-manager/modules/shell.nix
+++ home-manager/modules/shell.nix
@@ -22,6 +22,7 @@ in {
     sessionVariables = {
       RPS1 = ""; # Disable the right side prompt that "walters" theme introduces
       ZDOTDIR = "/home/pinpox/.config/zsh";
+      ZK_NOTEBOOK_DIR = "/home/pinpox/Notes";
       EDITOR = "nvim";
       VISUAL = "nvim";
     };
