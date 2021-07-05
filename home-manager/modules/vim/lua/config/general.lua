ddf779cbc1583f771effd91cf56ff92665a868ab
Pablo Ovelleiro Corral <github@pablo.tools>
2021-07-05 09:21:29 +0200

    Add comments to vim config

--- home-manager/modules/vim/lua/config/general.lua
+++ home-manager/modules/vim/lua/config/general.lua
@@ -6,7 +6,7 @@
 vim.o.autochdir        = true               -- Automatically change the working dir to current file
 vim.o.backspace        = "indent,eol,start" -- allow backspacing over everything in insert mode
 vim.o.clipboard        = "unnamedplus"      -- Set Clipboard to system's clipboard
-vim.o.completeopt      = "menuone,noselect" -- TODO: What does this do?
+vim.o.completeopt      = "menuone,noselect" -- Use popup completion menu for single results aswell
 vim.o.foldlevelstart   = 20                 -- start with open folds
 vim.o.foldmethod       = "syntax"           -- set folding based on syntax
 vim.o.hidden           = true               -- Hide buffer when, but don't unload
@@ -16,7 +16,7 @@ vim.o.ignorecase       = true               -- Case insensitive search
 vim.o.incsearch        = true               -- search as characters are entered
 vim.o.laststatus       = 2                  -- Always show the status line
 vim.o.lazyredraw       = true               -- redraw only when we need to.
-vim.o.mouse            = "a"                -- TODO: What does this do?
+vim.o.mouse            = "a"                -- Enable mouse in all modes
 vim.o.ruler            = true               -- show the cursor position all the time
 vim.o.scrolloff        = 5                  -- show lines above and below when scrolling
 vim.o.showcmd          = true               -- display incomplete commands
@@ -26,7 +26,7 @@ vim.o.title            = true               -- Show title in terminal window
 vim.o.wildmenu         = true               -- Complete commands
 vim.o.wrapscan         = true               -- Wrap when searching to beginning
 vim.o.conceallevel     = 0                  -- Don't ever hide stuff from me
-vim.o.inccommand       = "nosplit"          -- TODO: What does this do?
+vim.o.inccommand       = "nosplit"          -- Show command effects as you type
 vim.o.timeoutlen       = 500                -- " Set timeout, e.g. used in whichkey
 vim.g.mapleader        = " "                -- Set mapleader to space
 vim.o.cmdheight        = 1                  -- Height of the command section
@@ -48,14 +48,14 @@ vim.o.undofile         = true               -- Maintain undo history between ses
 
 -- Indention
 -- TODO look at: https://stackoverflow.com/questions/3682582/how-to-use-only-tab-not-space-in-vim
-vim.bo.shiftwidth       = 4                  -- Size of a tab
+vim.bo.shiftwidth      = 4                  -- Size of a tab
 vim.o.autoindent       = true               -- always set autoindenting on
 vim.bo.expandtab       = false              -- Don't expand tabs to spaces
 vim.o.softtabstop      = 4                  -- Size of a tab
 vim.o.tabstop          = 4                  -- A tab is displayed 4 collumns wide
 
 -- Language specific
-vim.g["go_auto_type_info"]      = 1          -- Go:        Show Go type info of variables
+vim.g["go_auto_type_info"]     = 1          -- Go:        Show Go type info of variables
 vim.g["vim_markdown_conceal"]  = 0          -- Markdown:  Disable concellevel for markdown
 vim.g["terraform_align"]       = 1          -- Terraform: Auto-align
 vim.g["terraform_fmt_on_save"] = 1          -- Terraform: Format on saving
