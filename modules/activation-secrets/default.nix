6d80fc3b9db212dc7e503d2509c7af93467d6ba4
Pablo Ovelleiro Corral <mail@pablo.tools>
2021-07-19 20:11:48 +0200

    Add activation-secrets module

--- modules/activation-secrets/default.nix
+++ modules/activation-secrets/default.nix
@@ -30,38 +30,38 @@ let
       };
       source-path = mkOption {
         type = types.str;
-        default = toString <secrets> + "/${config.name}";
+        default = "/var/src/secrets/${config.name}";
       };
     };
   });
 in {
-  # options.krops.secrets = {
-  #   files = mkOption {
-  #     type = with types; attrsOf secret-file;
-  #     default = {};
-  #   };
-  # };
-  # config = lib.mkIf (cfg.files != {}) {
-  #   system.activationScripts.setup-secrets = let
-  #     files = unique (map (flip removeAttrs ["_module"])
-  #                         (attrValues cfg.files));
-  #     script = ''
-  #       echo setting up secrets...
-  #       mkdir -p /run/keys -m 0750
-  #       chown root:keys /run/keys
-  #       ${concatMapStringsSep "\n" (file: ''
-  #         ${pkgs.coreutils}/bin/install \
-  #           -D \
-  #           --compare \
-  #           --verbose \
-  #           --mode=${lib.escapeShellArg file.mode} \
-  #           --owner=${lib.escapeShellArg file.owner} \
-  #           --group=${lib.escapeShellArg file.group-name} \
-  #           ${lib.escapeShellArg file.source-path} \
-  #           ${lib.escapeShellArg file.path} \
-  #         || echo "failed to copy ${file.source-path} to ${file.path}"
-  #       '') files}
-  #     '';
-  #   in stringAfter [ "users" "groups" ] "source ${pkgs.writeText "setup-secrets.sh" script}";
-  # };
+  options.krops.secrets = {
+    files = mkOption {
+      type = with types; attrsOf secret-file;
+      default = {};
+    };
+  };
+  config = lib.mkIf (cfg.files != {}) {
+    system.activationScripts.setup-secrets = let
+      files = unique (map (flip removeAttrs ["_module"])
+                          (attrValues cfg.files));
+      script = ''
+        echo setting up secrets...
+        mkdir -p /run/keys -m 0750
+        chown root:keys /run/keys
+        ${concatMapStringsSep "\n" (file: ''
+          ${pkgs.coreutils}/bin/install \
+            -D \
+            --compare \
+            --verbose \
+            --mode=${lib.escapeShellArg file.mode} \
+            --owner=${lib.escapeShellArg file.owner} \
+            --group=${lib.escapeShellArg file.group-name} \
+            ${lib.escapeShellArg file.source-path} \
+            ${lib.escapeShellArg file.path} \
+          || echo "failed to copy ${file.source-path} to ${file.path}"
+        '') files}
+      '';
+    in stringAfter [ "users" "groups" ] "source ${pkgs.writeText "setup-secrets.sh" script}";
+  };
 }
