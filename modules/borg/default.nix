fa73a63063aa3fa690e2954a04b92c9fe4d9419c
Pablo Ovelleiro Corral <mail@pablo.tools>
2021-07-28 20:08:01 +0200

    irc-announce: remove /dev/stderr

--- modules/borg/default.nix
+++ modules/borg/default.nix
@@ -64,14 +64,14 @@
     startAt = "daily";
 
     # Write information for last snapshot to be retrieved by the monitoring
-    # readWritePaths = [ "/dev/stderr" ];
+    # readWritePaths = [ "/dev/stderr" "/proc/self/fd/2" ];
 
     postCreate = ''
-    ${pkgs.nur.repos.mic92.irc-announce}/bin/irc-announce irc.hackint.org 6697 backup-reporter '#lounge-rocks-log' 1 "[${config.networking.hostName}] Backup created: $archiveName"
-      '';
+      ${pkgs.nur.repos.mic92.irc-announce}/bin/irc-announce irc.hackint.org 6697 backup-reporter '#lounge-rocks-log' 1 "[${config.networking.hostName}] Backup created: $archiveName"
+        '';
     postHook = ''
-    ${pkgs.nur.repos.mic92.irc-announce}/bin/irc-announce irc.hackint.org 6697 backup-reporter '#lounge-rocks-log' 1 "[${config.networking.hostName}] Backup finished with status $exitStatus"
-      '';
+      ${pkgs.nur.repos.mic92.irc-announce}/bin/irc-announce irc.hackint.org 6697 backup-reporter '#lounge-rocks-log' 1 "[${config.networking.hostName}] Backup finished with status $exitStatus"
+        '';
     #   borg info --json --last 1 borg@birne.wireguard:. > /var/log/borgbackup-last-info
     #   echo $exitStatus > /var/log/borgbackup-last-status
     #   '';
