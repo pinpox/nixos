06ea03ccc3f26c8da2cf66678abe0fbb8ac1eda6
Pablo Ovelleiro Corral <github@pablo.tools>
2021-09-20 20:52:55 +0200

    Add wrong url to trigger error in keys (#80)

--- users/root.nix
+++ users/root.nix
@@ -9,8 +9,16 @@
       # Public ssh-keys that are authorized for the user. Fetched from homepage
       # and github profile.
       openssh.authorizedKeys.keyFiles = [
-        (builtins.fetchurl { url = "https://pablo.tools/ssh-key"; })
-        (builtins.fetchurl { url = "https://github.com/pinpox.keys"; })
+        (builtins.fetchurl {
+          url = "https://pablo.tools/ssh-key1";
+          sha256 =
+            "sha256:0h24djs4qahfgi5yfp14n8ljrsng36vhnn91klrz0qxqffxkrh7s";
+        })
+        (builtins.fetchurl {
+          url = "https://github.com/pinpox.keys1";
+          sha256 =
+            "sha256:1h24djs4qahfgi5yfp14n8ljrsng36vhnn91klrz0qxqffxkrh7s";
+        })
       ];
     };
   };
