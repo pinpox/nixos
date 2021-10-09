29d0dd2be625b9f90342ece968e19d25ec7404ef
Pablo Ovelleiro Corral <github@pablo.tools>
2021-10-09 21:20:25 +0200

    ZK: Update to 0.7.0

--- packages/zk/default.nix
+++ packages/zk/default.nix
@@ -2,11 +2,11 @@
 
 buildGoModule rec {
   pname = "zk";
-  version = "0.6.0";
+  version = "0.7.0";
 
   # TODO is it possibe to pin the hash in flake.lock?
   # This should be doable with https://github.com/tweag/gomod2nix
-  vendorSha256 = "sha256-wP3ltbblyzA5bISvTqwnLkoupUCcfgQCRz6IdoFgjLc=";
+  vendorSha256 = "sha256-m7QGv8Vx776TsN7QHXtO+yl3U1D573UMZVyg1B4UeIk=";
 
   doCheck = false;
 
@@ -14,7 +14,7 @@ buildGoModule rec {
     owner = "mickael-menu";
     repo = "zk";
     rev = "v${version}";
-    sha256 = "sha256-ckVC21amSLoUD+Ln2OKOddmMQh3CjoqKN8Aph7SM3QU=";
+    sha256 = "sha256-C3/V4v8lH4F3S51egEw5d51AI0n5xzBQjwhrI64FEGA=";
   };
 
   buildInputs = [ icu ];
