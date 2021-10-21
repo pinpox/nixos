fe876fe8eb7e6e942db73ccb8439a400625ed4e4
Pablo Ovelleiro Corral <github@pablo.tools>
2021-10-21 09:15:38 +0200

    Comment out deprecated rofi opions

--- home-manager/modules/rofi/default.nix
+++ home-manager/modules/rofi/default.nix
@@ -19,51 +19,51 @@ in {
       # extraConfig = "";
 
       # Geometry
-      fullscreen = true;
-      borderWidth = 0;
-      lines = 3;
-      padding = 400;
-      rowHeight = 2;
+      # fullscreen = true;
+      # borderWidth = 0;
+      # lines = 3;
+      # padding = 400;
+      # rowHeight = 2;
 
-      colors = {
+      # colors = {
 
-        window = {
-          background = "#${vars.colors.Black}";
-          # foreground = "#${vars.colors.Black}";
-          separator = "#${vars.colors.Blue}";
-          border = "#${vars.colors.Black}";
-        };
+      #   window = {
+      #     background = "#${vars.colors.Black}";
+      #     # foreground = "#${vars.colors.Black}";
+      #     separator = "#${vars.colors.Blue}";
+      #     border = "#${vars.colors.Black}";
+      #   };
 
-        rows = {
-          normal = {
-            background = "#${vars.colors.Black}";
-            foreground = "#${vars.colors.White}";
-            backgroundAlt = "#${vars.colors.Black}";
-            highlight = {
-              background = "#${vars.colors.Grey}";
-              foreground = "#${vars.colors.DarkGreen}";
-            };
-          };
-          active = {
-            background = "#${vars.colors.Black}";
-            foreground = "#${vars.colors.Blue}";
-            backgroundAlt = "#${vars.colors.Black}";
-            highlight = {
-              background = "#${vars.colors.Black}";
-              foreground = "#${vars.colors.Blue}";
-            };
-          };
-          urgent = {
-            background = "#${vars.colors.Black}";
-            foreground = "#${vars.colors.Red}";
-            backgroundAlt = "#${vars.colors.Black}";
-            highlight = {
-              background = "#${vars.colors.Black}";
-              foreground = "#${vars.colors.Red}";
-            };
-          };
-        };
-      };
+      #   rows = {
+      #     normal = {
+      #       background = "#${vars.colors.Black}";
+      #       foreground = "#${vars.colors.White}";
+      #       backgroundAlt = "#${vars.colors.Black}";
+      #       highlight = {
+      #         background = "#${vars.colors.Grey}";
+      #         foreground = "#${vars.colors.DarkGreen}";
+      #       };
+      #     };
+      #     active = {
+      #       background = "#${vars.colors.Black}";
+      #       foreground = "#${vars.colors.Blue}";
+      #       backgroundAlt = "#${vars.colors.Black}";
+      #       highlight = {
+      #         background = "#${vars.colors.Black}";
+      #         foreground = "#${vars.colors.Blue}";
+      #       };
+      #     };
+      #     urgent = {
+      #       background = "#${vars.colors.Black}";
+      #       foreground = "#${vars.colors.Red}";
+      #       backgroundAlt = "#${vars.colors.Black}";
+      #       highlight = {
+      #         background = "#${vars.colors.Black}";
+      #         foreground = "#${vars.colors.Red}";
+      #       };
+      #     };
+      #   };
+      # };
     };
   };
 }
