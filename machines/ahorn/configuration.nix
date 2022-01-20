a980f3c69d383aa5e5786441c2f1126077b16619
Pablo Ovelleiro Corral <mail@pablo.tools>
2022-01-20 18:51:35 +0100

    Add telegraf config to ahorn

--- machines/ahorn/configuration.nix
+++ machines/ahorn/configuration.nix
@@ -5,7 +5,7 @@
   imports = [ ./hardware-configuration.nix ./retiolum.nix ];
 
   networking.retiolum.ipv4 = "10.243.100.100";
-  networking.retiolum.ipv6 = "42:0:3c46:519d:1696:f464:9756:8727" ;
+  networking.retiolum.ipv6 = "42:0:3c46:519d:1696:f464:9756:8727";
 
   services.tinc.networks.retiolum = {
     rsaPrivateKeyFile = "/var/src/secrets/retiolum/rsa_priv";
@@ -17,6 +17,59 @@
   # '';
 
   # services.vault.package = pkgs.vault-bin;
+
+  services.telegraf = {
+    enable = true;
+    extraConfig = {
+      agent.interval = "60s";
+      inputs = {
+
+        #        #syslog.server = "unixgram:///run/systemd/journal/syslog";
+        #        #syslog.best_effort = true;
+        #        #syslog.syslog_standard = "RFC3164";
+        #        prometheus.urls = lib.mkIf (config.services.promtail.enable) [
+        #          # default promtail port
+        #          "http://localhost:9080/metrics"
+        #        ];
+        #        prometheus.metric_version = 2;
+        #        smart = lib.mkIf (!isVM) {
+        #          path = pkgs.writeShellScript "smartctl" ''
+        #            exec /run/wrappers/bin/sudo ${pkgs.smartmontools}/bin/smartctl "$@"
+        #          '';
+        #        };
+        #        file = [{
+        #          data_format = "influx";
+        #          file_tag = "name";
+        #          files = [ "/var/log/telegraf/*" ];
+        #        }] ++ lib.optional (lib.any (fs: fs == "ext4") config.boot.supportedFilesystems) {
+        #          name_override = "ext4_errors";
+        #          files = [ "/sys/fs/ext4/*/errors_count" ];
+        #          data_format = "value";
+        #        };
+        #        disk.tagdrop = {
+        #          fstype = [ "tmpfs" "ramfs" "devtmpfs" "devfs" "iso9660" "overlay" "aufs" "squashfs" ];
+        #          device = [ "rpc_pipefs" "lxcfs" "nsfs" "borgfs" ];
+        #        };
+
+        mdstat = { };
+        system = { };
+        mem = { };
+        kernel_vmstat = { };
+        systemd_units = { };
+        swap = { };
+        diskio = { };
+      };
+      outputs = {
+
+        prometheus_client = {
+          listen = ":9273";
+          metric_version = 2;
+        };
+      };
+
+    };
+
+  };
 
   boot.blacklistedKernelModules = [ "nouveau" ];
 
