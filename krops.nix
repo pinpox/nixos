80fb6154a12cd084fc00cd75cc48ef8b5a16d733
Pablo Ovelleiro Corral <mail@pablo.tools>
2022-03-05 11:06:48 +0100

    Update bob port

--- krops.nix
+++ krops.nix
@@ -51,7 +51,7 @@ in rec {
   # Individual machines
   ahorn = createHost "ahorn" "root@192.168.7.2";
   birne = createHost "birne" "root@192.168.2.84";
-  bob = createHost "bob" "root@drone.lounge.rocks:2222";
+  bob = createHost "bob" "root@drone.lounge.rocks";
   kartoffel = createHost "kartoffel" "root@kartoffel.wireguard";
   kfbox = createHost "kfbox" "root@46.38.242.17";
   mega = createHost "mega" "root@mega.public";
