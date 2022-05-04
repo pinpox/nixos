e4d6a41205700eee499c667a50a321b24c4ddc07
Pablo Ovelleiro Corral <mail@pablo.tools>
2022-05-04 12:49:07 +0200

    Add session

--- modules/xserver/default.nix
+++ modules/xserver/default.nix
@@ -1,6 +1,24 @@
 { config, pkgs, lib, ... }:
 with lib;
-let cfg = config.pinpox.services.xserver;
+let
+  cfg = config.pinpox.services.xserver;
+  # i3kde =
+
+  #   pkgs.writeTextFile {
+  #     name = "plasma-i3-xsession";
+  #     destination = "/share/xsessions/plasma-i3.desktop";
+  #     text = ''
+  #       [Desktop Entry]
+  #       Type=XSession
+  #       Exec=${pkgs.coreutils}/bin/env KDEWM=${pkgs.i3}/bin/i3 /not/sure/what/this/is/bin/startplasma-x11
+  #       DesktopNames=KDE
+  #       Name=Plasma with i3
+  #       Comment=Plasma with i3
+  #     '';
+
+  #   } //{
+  #     passthru = { providedSessions = [ "sm.puri.Phosh" ]; };
+  #   } ;
 in {
 
   options.pinpox.services.xserver = { enable = mkEnableOption "X server"; };
@@ -30,18 +48,35 @@ in {
         EndSection
       '';
 
-      displayManager.startx.enable = true;
+      # displayManager.startx.enable = true;
 
-      desktopManager = {
-        xterm.enable = false;
+      # displayManager.sddm.enable = true;
+
+      displayManager = {
+        sddm.enable = true;
+        defaultSession = "myplasmai3session";
         session = [{
-          name = "home-manager";
-          start = ''
-             ${pkgs.runtimeShell} $HOME/.hm-xsession &
-            waitPID=$!
-          '';
+          manage = "desktop";
+          name = "myplasmai3session";
+          start =
+            "exec env KDEWM=${pkgs.i3-gaps}/bin/i3 ${pkgs.plasma-workspace}/bin/startplasma-x11";
         }];
       };
+
+      # displayManager.extraSessionFilesPackages = [i3kde];
+      desktopManager.plasma5.enable = true;
+      windowManager.i3.enable = true;
+
+      # desktopManager = {
+      #   xterm.enable = false;
+      #   session = [{
+      #     name = "home-manager";
+      #     start = ''
+      #        ${pkgs.runtimeShell} $HOME/.hm-xsession &
+      #       waitPID=$!
+      #     '';
+      #   }];
+      # };
     };
   };
 }
