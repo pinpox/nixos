042049e9f933be068fe8d055038a4b9a45af1ac5
Pablo Ovelleiro Corral <mail@pablo.tools>
2022-05-22 14:10:39 +0200

    add enable option to awesome hm mod

---
+++ home-manager/modules/awesome/default.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, lib, ... }:
+with lib;
+let
+  vars = import ./vars.nix;
+  cfg = config.pinpox.programs.awesome;
+in
+{
+  options.pinpox.programs.awesome.enable = mkEnableOption "awesome window manager";
+
+  config = mkIf cfg.enable {
+
+    # imports = [ dotfiles-awesome.dotfiles ];
+
+    xsession.scriptPath = ".hm-xsession";
+    xsession.enable = true;
+
+    xsession.windowManager.awesome = {
+      enable = true;
+      package = pkgs.awesome;
+
+      # List of lua packages available for being used in the Awesome
+      # configuration.
+      luaModules = [ pkgs.luaPackages.lgi pkgs.luaPackages.luafilesystem ];
+
+      # Disable client transparency support, which can be greatly detrimental to
+      # performance in some setups
+      # noArgd = true;
+    };
+  };
+}
