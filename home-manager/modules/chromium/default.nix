391e023ef5251433d7aea1c4fe3e50e751a7b772
Pablo Ovelleiro Corral <mail@pablo.tools>
2022-05-23 18:48:45 +0200

    add enable option to chromium config

---
+++ home-manager/modules/chromium/default.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, lib, ... }: {
+  with lib;
+  let
+  vars = import ../vars.nix;
+  cfg = config.pinpox.programs.chromium;
+  in
+  {
+  options.pinpox.programs.chromium.enable = mkEnableOption "chromium browser";
+
+  config = mkIf cfg.enable {
+    programs.chromium = {
+      enable = true;
+      extensions = [
+        { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
+        { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # Ublock Origin
+        { id = "gcbommkclmclpchllfjekcdonpmejbdp"; } # HTTPS everywhere
+        { id = "mmpokgfcmbkfdeibafoafkiijdbfblfg"; } # Merge windows
+      ];
+    };
+  };
+}
