c07c9998255f2af890ca6cfdda0560469aa9b76d
Pablo Ovelleiro Corral <mail@pablo.tools>
2022-11-01 17:41:19 +0100

    Add ntfy for borg and restic

--- modules/borg/default.nix
+++ modules/borg/default.nix
@@ -12,7 +12,10 @@ in
     lollypops.secrets.files = {
       "borg/passphrase" = { };
       "ssh/borg/private" = { };
+      "ntfy/borg" = { };
     };
+
+    systemd.services.borgbackup-job-box-backup.serviceConfig.EnvironmentFile = [ config.lollypops.secrets.files."ntfy/borg".path ];
 
     # Backup with borgbackup to remote server. The connection key and repository
     # encryption passphrase is read from /secrets. This directory has to be
@@ -90,22 +93,24 @@ in
       #     https://vpn.notify.pablo.tools/plain
       #   ${pkgs.nur.repos.mic92.irc-announce}/bin/irc-announce irc.hackint.org 6697 backup-reporter '#lounge-rocks-log' 1 "💾 [${config.networking.hostName}] Backup created: $archiveName"
       # '';
-      # postHook = ''
-      #   if [ $exitStatus -ne 0 ]; then
-      #     ${pkgs.curl}/bin/curl -X POST \
-      #       -d"<p>💾 <strong><font color='#ff0000'>BACKUP</font> </strong><code>[${config.networking.hostName}]</code> >> FAILED!</br>\
-      #       <blockquote>Archive: $archiveName</br>Status: $exitStatus</blockquote>" \
-      #       https://vpn.notify.pablo.tools/plain
-      #   else
-      #     ${pkgs.curl}/bin/curl -X POST \
-      #       -d"<p>💾 <strong><font color='#0000ff'>BACKUP</font> </strong><code>[${config.networking.hostName}]</code> >> Created successfully</br>\
-      #       <blockquote>Archive: $archiveName</br>Status: $exitStatus</blockquote>" \
-      #       https://vpn.notify.pablo.tools/plain
-      #   fi
-
-      #   echo $?
-      #   exit $exitStatus
-      # '';
+      postHook =
+        let
+          host = config.networking.hostName;
+        in
+        ''
+          if [ $exitStatus -ne 0 ]; then
+            ${pkgs.curl}/bin/curl -u $NTFY_USER:$NTFY_PASS \
+            -H 'Title: Backup on ${host} failed!' \
+            -H 'Tags: backup,borg,${host}' \
+            -d "Backup error on ${host}: $exitStatus" 'https://push.pablo.tools/pinpox_backups'
+          else
+            ${pkgs.curl}/bin/curl -u $NTFY_USER:$NTFY_PASS \
+            -H 'Title: Backup on ${host} successful!' \
+            -H 'Tags: backup,borg,${host}' \
+            -d "Backup success on ${host}: $exitStatus $archiveName" 'https://push.pablo.tools/pinpox_backups'
+          fi
+          exit $exitStatus
+        '';
       #   borg info --json --last 1 borg@birne.wireguard:. > /var/log/borgbackup-last-info
       #   echo $exitStatus > /var/log/borgbackup-last-status
       #   '';
--- modules/restic/default.nix
+++ modules/restic/default.nix
@@ -11,17 +11,34 @@ in
   config = mkIf cfg.enable {
 
     lollypops.secrets.files = {
-      "restic/s3-credentials" = { };
+      "restic/credentials" = { };
       "restic/repo-pw" = { };
     };
 
-    services.restic.backups = {
-      s3-pinpox = {
-        paths = [ "/home/pinpox/Notes/" ];
-        repository = "s3:https://vpn.s3.pablo.tools/restic";
-        environmentFile = "${config.lollypops.secrets.files."restic/s3-credentials".path}";
-        passwordFile = "${config.lollypops.secrets.files."restic/repo-pw".path}";
+    services.restic.backups =
+      let
+        host = config.networking.hostName;
+      in
+      {
+        s3-pinpox = {
+          paths = [ "/home/pinpox/Notes/" ];
+          repository = "s3:https://vpn.s3.pablo.tools/restic";
+          environmentFile = "${config.lollypops.secrets.files."restic/credentials".path}";
+          passwordFile = "${config.lollypops.secrets.files."restic/repo-pw".path}";
+          backupCleanupCommand = ''
+            if [ $EXIT_STATUS -ne 0 ]; then
+              ${pkgs.curl}/bin/curl -u $NTFY_USER:$NTFY_PASS \
+              -H 'Title: Backup on ${host} failed!' \
+              -H 'Tags: backup,borg,${host}' \
+              -d "Restic backup error on ${host}!" 'https://push.pablo.tools/pinpox_backups'
+            else
+              ${pkgs.curl}/bin/curl -u $NTFY_USER:$NTFY_PASS \
+              -H 'Title: Backup on ${host} successful!' \
+              -H 'Tags: backup,borg,${host}' \
+              -d "Restic backup success on ${host}!" 'https://push.pablo.tools/pinpox_backups'
+            fi
+          '';
+        };
       };
-    };
   };
 }
