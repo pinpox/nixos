06dc7991b2eb8857036941fa29e8ef40b32050dc
Pablo Ovelleiro Corral <mail@pablo.tools>
2022-12-11 13:03:57 +0100

    Re-enable *-wait-online on ahorn

--- machines/ahorn/configuration.nix
+++ machines/ahorn/configuration.nix
@@ -8,8 +8,8 @@
   ];
 
   # often hangs
-  systemd.services.systemd-networkd-wait-online.enable = false;
-  systemd.services.NetworkManager-wait-online.enable = false;
+  # systemd.services.systemd-networkd-wait-online.enable = false;
+  # systemd.services.NetworkManager-wait-online.enable = false;
 
   lollypops = {
 
