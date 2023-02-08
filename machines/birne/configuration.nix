7df16375d1cf4c3ec703104de971f92a285cc092
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-02-08 15:46:33 +0100

    Fix unifi controller on birne

--- machines/birne/configuration.nix
+++ machines/birne/configuration.nix
@@ -98,8 +98,25 @@
   security.acme.acceptTerms = true;
   security.acme.defaults.email = "letsencrypt@pablo.tools";
 
-  services.unifi.enable = true;
-  services.unifi.unifiPackage = pkgs.unifi6;
+  # Access locally via:
+  # https://birne:8443/manage/
+  services.unifi = {
+    enable = true;
+
+    # 6 is latest supported for my access points. Beware that this will build
+    # an older version of mongodb (from source), which may cause slow rebuilds
+    # if it's not cached.
+    unifiPackage = pkgs.unifi6;
+
+    # Open required ports
+    openFirewall = true;
+    # tcp/8080  # Port for UAP to inform controller.
+    # tcp/8880  # Port for HTTP portal redirect, if guest portal is enabled.
+    # tcp/8843  # Port for HTTPS portal redirect, ditto.
+    # tcp/6789  # Port for UniFi mobile speed test.
+    # udp/3478  # UDP port used for STUN.
+    # udp/10001 # UDP port used for device discovery.
+  };
 
   # Open ports in the firewall.
   networking.firewall = {
@@ -109,8 +126,6 @@
       80
       443
       4533
-      8080
-      8443
     ];
   };
 }
