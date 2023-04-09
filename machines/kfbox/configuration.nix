c836fdeea16e8af3f9e5d71c2b8a5528f3e9c6bf
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-04-09 19:25:00 +0200

    kbox: remove old mattermost config comments

--- machines/kfbox/configuration.nix
+++ machines/kfbox/configuration.nix
@@ -267,12 +267,6 @@
         locations."/" = { proxyPass = "http://127.0.0.1:9090"; };
       };
 
-      # "chat.0cx.de" = {
-      #   forceSSL = true;
-      #   enableACME = true;
-      #   root = self.inputs.nixpkgs.legacyPackages.x86_64-linux.cinny;
-      # };
-
       # Pads
       "pads.0cx.de" = {
         forceSSL = true;
@@ -299,61 +293,6 @@
           '';
         };
       };
-
-      # # Mattermost
-      # "mm.0cx.de" = {
-      #   forceSSL = true;
-      #   enableACME = true;
-      #   locations."/" = {
-      #     proxyPass = "http://127.0.0.1:8065";
-      #     proxyWebsockets = true;
-
-      #     extraConfig = ''
-      #       proxy_pass_request_headers on;
-      #       add_header Access-Control-Allow-Origin *;
-      #       client_max_body_size 50M;
-      #       proxy_set_header Connection "";
-      #       proxy_set_header Host $host;
-      #       proxy_set_header X-Real-IP $remote_addr;
-      #       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-      #       proxy_set_header X-Forwarded-Proto $scheme;
-      #       proxy_set_header X-Frame-Options SAMEORIGIN;
-      #       proxy_buffers 256 16k;
-      #       proxy_buffer_size 16k;
-      #       proxy_read_timeout 600s;
-      #       proxy_cache_revalidate on;
-      #       proxy_cache_min_uses 2;
-      #       proxy_cache_use_stale timeout;
-      #       proxy_cache_lock on;
-      #     '';
-      #   };
-
-      #   locations."~ /api/v[0-9]+/(users/)?websocket$" = {
-      #     extraConfig = ''
-      #       proxy_pass_request_headers on;
-      #       add_header Access-Control-Allow-Origin *;
-      #       proxy_set_header Upgrade $http_upgrade;
-      #       proxy_set_header Connection "upgrade";
-      #       client_max_body_size 50M;
-      #       proxy_set_header Host $host;
-      #       proxy_set_header X-Real-IP $remote_addr;
-      #       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-      #       proxy_set_header X-Forwarded-Proto $scheme;
-      #       proxy_set_header X-Frame-Options SAMEORIGIN;
-      #       proxy_buffers 256 16k;
-      #       proxy_buffer_size 16k;
-      #       client_body_timeout 60;
-      #       send_timeout 300;
-      #       lingering_timeout 5;
-      #       proxy_connect_timeout 90;
-      #       proxy_send_timeout 300;
-      #       proxy_read_timeout 90s;
-      #     '';
-
-      #     proxyPass = "http://127.0.0.1:8065";
-      #     proxyWebsockets = true;
-      #   };
-      # };
     };
   };
 }
