3bc69cde4f45e6d0f09224fa6258e978d234920f
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-05-04 10:12:09 +0200

    working html manual

--- flake.nix
+++ flake.nix
@@ -309,25 +309,28 @@
               # nix build '.#flake-manual' --show-trace && cat result | jq
               flake-manual =
                 let
+
                   isValidOpt = a: (builtins.hasAttr "_type" a) && (a._type == "option")
                   && (builtins.hasAttr "default" a)
                   && (builtins.hasAttr "example" a)
                   && (builtins.hasAttr "description" a)
                   && (builtins.hasAttr "type" a);
 
-                  getOptionValues = opt:
+                  getOptionValues = opt: path:
                     if builtins.typeOf opt == "set" then
                       if isValidOpt opt then
                         {
-                          declaredIn = "TODO";
+                          inherit path;
+                          name = builtins.concatStringsSep "." path;
                           example = opt.example;
                           description = opt.description;
                           default = opt.default;
                           type = opt.type.description;
+                          documentedOption = true;
                         }
                       else
                       # it is a set, but has no "default", recurse
-                        builtins.mapAttrs (name: value: getOptionValues value) opt
+                        builtins.mapAttrs (name: value: getOptionValues value (path ++ [ "${name}" ])) opt
                     else
                       { }; # it is no set
                 in
@@ -336,28 +339,32 @@
                     name = "options.json";
                     text = builtins.toJSON
 
-                      (pkgs.lib.attrsets.mapAttrs
-                        (name: value:
-                          let
-                            allopts = getOptionValues (value ({
-                              inherit (inputs) flake-self;
-                              inherit pkgs;
-                              lib = pkgs.lib;
-                              config = { };
-                            } // inputs)
-                            );
-                          in
-                          if
-                          # Filter out everything that has no ".options.pinpox"
-                            builtins.hasAttr "options" allopts then
-                            if builtins.hasAttr "pinpox" allopts.options then
-                              allopts.options.pinpox
-                            else null
-                          else
-                            null
-                        )
-                        self.nixosModules
-                      );
+                      {
+                        options = pkgs.lib.attrsets.collect (o: o ? "documentedOption")
+                          (pkgs.lib.attrsets.mapAttrs
+                            (name: value:
+                              let
+                                allopts = getOptionValues
+                                  (value ({
+                                    inherit (inputs) flake-self;
+                                    inherit pkgs;
+                                    lib = pkgs.lib;
+                                    config = { };
+                                  } // inputs)
+                                  ) [ ];
+                              in
+                              if
+                              # Filter out everything that has no ".options.pinpox"
+                                builtins.hasAttr "options" allopts then
+                                if builtins.hasAttr "pinpox" allopts.options then
+                                  allopts.options.pinpox
+                                else null
+                              else
+                                null
+                            )
+                            self.nixosModules
+                          );
+                      };
                   };
             };
 
