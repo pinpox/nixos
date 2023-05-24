1a8238f304ace4504ecc2ddd2e38c001b64864c6
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-05-24 12:43:39 +0200

    Prettify colorscheme module

--- home-manager/colorscheme.nix
+++ home-manager/colorscheme.nix
@@ -1,83 +1,54 @@
 { lib, ... }:
 with lib;
+let
+  colornames = [
+    "Black"
+    "BrightBlack"
+    "White"
+    "BrightWhite"
+    "Yellow"
+    "BrightYellow"
+    "Green"
+    "BrightGreen"
+    "Cyan"
+    "BrightCyan"
+    "Blue"
+    "BrightBlue"
+    "Magenta"
+    "BrightMagenta"
+    "Red"
+    "BrightRed"
+  ];
+in
 {
 
   options.pinpox = {
-    colors = {
-      # enable = mkEnableOption "foot terminal emulator";
-
-      Black = mkOption {
-        type = types.str;
-        default = "000000";
-      };
-
-      # White = "FDF7CD"; # #FDF7CD
-      # DarkGreen = "55D1B4"; # #55D1B4
-      # DarkMagenta = "8B66A4"; # #8B66A4
-      # DarkCyan = "6EB1CC"; # #6EB1CC
-
-      BrightBlack = mkOption {
-        type = types.str;
-        default = "2c2d3a"; #2c2d3a
-      };
-      White = mkOption {
-        type = types.str;
-        default = "fefbe6"; #fefbe6
-      };
-      BrightWhite = mkOption {
-        type = types.str;
-        default = "ECEFF4"; #ECEFF4
-      };
-      Yellow = mkOption {
-        type = types.str;
-        default = "ff9900"; #ff9900
-      };
-      BrightYellow = mkOption {
-        type = types.str;
-        default = "e1dc3f"; #e1dc3f
-      };
-      Green = mkOption {
-        type = types.str;
-        default = "3ec840"; #3ec840
-      };
-      BrightGreen = mkOption {
-        type = types.str;
-        default = "68f288"; #68f288
-      };
-      Cyan = mkOption {
-        type = types.str;
-        default = "00ecd8"; #00ecd8
-      };
-      BrightCyan = mkOption {
-        type = types.str;
-        default = "A1E4FF"; #A1E4FF
-      };
-      Blue = mkOption {
-        type = types.str;
-        default = "418fdd"; #418fdd
-      };
-      BrightBlue = mkOption {
-        type = types.str;
-        default = "5B77B3"; #5B77B3
-      };
-      Magenta = mkOption {
-        type = types.str;
-        default = "ff00cc"; #ff00cc
-      };
-      BrightMagenta = mkOption {
-        type = types.str;
-        default = "D8B3F0"; #D8B3F0
-      };
-      Red = mkOption {
-        type = types.str;
-        default = "e92741"; #e92741
-      };
-      BrightRed = mkOption {
-        type = types.str;
-        default = "FF5370"; #FF5370
-      };
-    };
+    colors = builtins.listToAttrs (map
+      (c: {
+        name = c;
+        value = mkOption { type = types.str; };
+      })
+      colornames);
   };
 
-  config = { };
+  config.pinpox.colors = {
+
+    Black = "000000"; #000000
+    BrightBlack = "2c2d3a"; #2c2d3a
+    White = "fefbe6"; #fefbe6
+    BrightWhite = "ECEFF4"; #ECEFF4
+    Yellow = "ff9900"; #ff9900
+    BrightYellow = "e1dc3f"; #e1dc3f
+    Green = "3ec840"; #3ec840
+    BrightGreen = "68f288"; #68f288
+    Cyan = "00ecd8"; #00ecd8
+    BrightCyan = "A1E4FF"; #A1E4FF
+    Blue = "418fdd"; #418fdd
+    BrightBlue = "5B77B3"; #5B77B3
+    Magenta = "ff00cc"; #ff00cc
+    BrightMagenta = "D8B3F0"; #D8B3F0
+    Red = "e92741"; #e92741
+    BrightRed = "FF5370"; #FF5370
+
+  };
 }
