517be8f02e0254ac4895cee24fc0f0ab5880d6a5
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-05-31 08:54:27 +0200

    fmt cmp config

--- home-manager/modules/nvim/lua/config/cmp.lua
+++ home-manager/modules/nvim/lua/config/cmp.lua
@@ -14,7 +14,7 @@ cmp.setup({
 		snippet = {
 				-- REQUIRED - you must specify a snippet engine
 				expand = function(args)
-						require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+					require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
 				end,
 		},
 		mapping = {
