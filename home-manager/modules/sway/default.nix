3cb30bafbf1d5c475c0ed8428eba07757ecd33db
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-08-19 16:42:28 +0200

    Launch sway without dbus-launch

--- home-manager/modules/sway/default.nix
+++ home-manager/modules/sway/default.nix
@@ -6,7 +6,8 @@ let
   start-sway = pkgs.writeShellScriptBin "start-sway" /* sh */
     ''
       export WLR_DRM_NO_MODIFIERS=1
-      dbus-launch --sh-syntax --exit-with-session ${pkgs.sway}/bin/sway
+      # dbus-launch --sh-syntax --exit-with-session ${pkgs.sway}/bin/sway
+      ${pkgs.sway}/bin/sway
     '';
 in
 {
