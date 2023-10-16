844e955f2c91c91f13a36d20b2e472cf5dafa680
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-10-16 12:54:00 +0200

    indent-blankline: fix deprected option

--- home-manager/modules/nvim/lua/config/appearance.lua
+++ home-manager/modules/nvim/lua/config/appearance.lua
@@ -7,9 +7,9 @@ vim.opt.listchars:append "space:⋅"
 -- vim.opt.listchars:append "eol:↴"
 
 require("ibl").setup {
-    space_char_blanklin = " ",
-    show_current_context = true,
-    show_current_context_start = false,
+    -- space_char_blankline = " ",
+    -- show_current_context = true,
+    -- show_current_context_start = false,
 }
 
 require'colorizer'.setup()
