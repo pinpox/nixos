ba8e40a9c55d4d270430bfbc4bf37d2418f4fca2
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-10-30 10:12:05 +0100

    Workaround for vaultwarden
    Revert when https://github.com/dani-garcia/vaultwarden/discussions/3996
    reaches release

--- modules/vaultwarden/default.nix
+++ modules/vaultwarden/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ pkgs, config, lib, ... }:
 with lib;
 let cfg = config.pinpox.services.vaultwarden;
 
@@ -18,11 +18,48 @@ in
 
   config = mkIf cfg.enable {
 
-    services.caddy = {
-      enable = true;
-      virtualHosts."${cfg.host}".extraConfig =
-        "reverse_proxy 127.0.0.1:${builtins.toString config.services.vaultwarden.config.ROCKET_PORT}";
-    };
+    services.caddy =
+      let
+
+        # TODO Workaround for vaultwarden until the next release.
+        # The current workaround will not be necessary/not work when this is released:
+        # https://github.com/dani-garcia/vaultwarden/discussions/3996
+        # Talked to @BlackDex on matrix.
+
+        config_workfile = pkgs.writeTextFile {
+          name = "config.json";
+          text = ''
+            {
+              "environment": {
+                "api": "https://pass.pablo.tools/api",
+                "identity": "https://pass.pablo.tools/identity",
+                "notifications": "https://pass.pablo.tools/notifications",
+                "sso": "",
+                "vault": "https://pass.pablo.tools"
+              },
+              "gitHash": "b707d616",
+              "object": "config",
+              "server": {
+                "name": "Vaultwarden",
+                "url": "https://github.com/dani-garcia/vaultwarden"
+              },
+              "version": "2023.9.0"
+            }
+          '';
+        };
+      in
+      {
+        enable = true;
+        virtualHosts."${cfg.host}".extraConfig =
+          ''
+            handle /api/config/ {
+              try_files {path} ${config_workfile}
+              file_server
+            }
+
+            reverse_proxy 127.0.0.1:${builtins.toString config.services.vaultwarden.config.ROCKET_PORT}
+          '';
+      };
 
     systemd.services.backup-vaultwarden.serviceConfig.StateDirectory = "vaultwarden-backups";
 
