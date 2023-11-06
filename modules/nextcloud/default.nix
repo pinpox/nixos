865170e09c1c52c25487e84df7a1c13cdb97ab36
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-11-06 13:45:06 +0100

    Nextcloud: add preview for more filetypes

--- modules/nextcloud/default.nix
+++ modules/nextcloud/default.nix
@@ -80,6 +80,21 @@ in
       nginx.recommendedHttpHeaders = true;
 
 
+      extraOptions.enabledPreviewProviders = [
+        "OC\\Preview\\BMP"
+        "OC\\Preview\\GIF"
+        "OC\\Preview\\JPEG"
+        "OC\\Preview\\Krita"
+        "OC\\Preview\\MarkDown"
+        "OC\\Preview\\MP3"
+        "OC\\Preview\\OpenDocument"
+        "OC\\Preview\\PNG"
+        "OC\\Preview\\TXT"
+        "OC\\Preview\\XBitmap"
+        "OC\\Preview\\HEIC"
+      ];
+
+
     };
 
 
