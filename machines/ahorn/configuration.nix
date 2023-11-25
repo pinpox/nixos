1cfb3bb4fb40cb5181074dc6546eaa6947078baa
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-11-25 17:27:36 +0100

    ahorn: enable focusrite gen3 driver

--- machines/ahorn/configuration.nix
+++ machines/ahorn/configuration.nix
@@ -1,6 +1,13 @@
 # Configuration file for ahorn
 { config, retiolum, pkgs, lib, nixos-hardware, ... }: {
 
+  # Enable driver for Focusrite Scarlett 2i2 Gen3.
+  # This can be removed when we reach Linux Kernel 6.7, as it includes the driver by default
+  # See: https://github.com/geoffreybennett/alsa-scarlett-gui/blob/master/INSTALL.md#enabling-the-driver
+  boot.extraModprobeConfig = ''
+    options snd_usb_audio vid=0x1235 pid=0x8210 device_setup=1
+  '';
+
 
   boot.extraModulePackages = [
     config.boot.kernelPackages.v4l2loopback
