5edc8be75267512cb8f0777ef80964dc481736bf
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-11-28 13:31:13 +0100

    Simplify vst plugins

--- home-manager/modules/audio-recording/default.nix
+++ home-manager/modules/audio-recording/default.nix
@@ -2,76 +2,50 @@
 with lib;
 let
   cfg = config.pinpox.defaults.audio-recording;
+  plugins = with pkgs; [
+    # Instruments
+    x42-avldrums
+    zynaddsubfx
+
+    # Effects
+    calf
+    lsp-plugins
+    distrho
+    zam-plugins
+    talentedhack
+    gxplugins-lv2
+  ];
 in
 {
 
+  # These settings yield 10ms@1024 spls latency in reaper for me:
+  # pw-metadata -n settings 0 clock.force-quantum 1024
+  # pw-metadata -n settings 0 clock.force-rate 96000
+
   options.pinpox.defaults.audio-recording.enable = mkEnableOption "audio production setup (DAW and plugins)";
 
-  config =
-    let
+  config = mkIf cfg.enable {
 
-      plugins = with pkgs; [
-        # Instruments
-        x42-avldrums
-        zynaddsubfx
+    home.packages = with pkgs; [ reaper ]
 
-        # Effects
-        calf
-        lsp-plugins
-        distrho
-        zam-plugins
-        talentedhack
-        gxplugins-lv2
-      ];
+      # Some plugins also have a binary, so we also add them to PATH
+      ++ plugins;
 
-    in
-    mkIf cfg.enable {
-
-      home.packages = with pkgs; [ reaper ]
-
-        # Some plugins also have a binary, so we also add them to PATH
-        ++ plugins;
-
-      # Place vst, vst3, clap, lv2 and ladspa plugins in the according
-      # directories where reaper will look for them
-      home.file =
-        let
-          all-audio-plugins = pkgs.symlinkJoin {
-            name = "all-audio-plugins";
-            paths = plugins;
-          };
-        in
-        {
-
-          all-lv2 = {
-            recursive = true;
-            source = "${all-audio-plugins}/lib/lv2";
-            target = ".lv2";
-          };
-
-          all-clap = {
-            recursive = true;
-            source = "${all-audio-plugins}/lib/clap";
-            target = ".clap";
-          };
-
-          all-vst = {
-            recursive = true;
-            source = "${all-audio-plugins}/lib/vst";
-            target = ".vst";
-          };
-
-          all-vst3 = {
-            recursive = true;
-            source = "${all-audio-plugins}/lib/vst3";
-            target = ".vst3";
-          };
-
-          all-ladspa = {
-            recursive = true;
-            source = "${all-audio-plugins}/lib/ladspa";
-            target = ".ladspa";
-          };
+    # Place vst, vst3, clap, lv2 and ladspa plugins in the according
+    # directories where reaper will look for them
+    home.file =
+      let
+        all-audio-plugins = pkgs.symlinkJoin {
+          name = "all-audio-plugins";
+          paths = plugins;
         };
-    };
+      in
+      {
+        all-lv2 = { recursive = true; source = "${all-audio-plugins}/lib/lv2"; target = ".lv2"; };
+        all-clap = { recursive = true; source = "${all-audio-plugins}/lib/clap"; target = ".clap"; };
+        all-vst = { recursive = true; source = "${all-audio-plugins}/lib/vst"; target = ".vst"; };
+        all-vst3 = { recursive = true; source = "${all-audio-plugins}/lib/vst3"; target = ".vst3"; };
+        all-ladspa = { recursive = true; source = "${all-audio-plugins}/lib/ladspa"; target = ".ladspa"; };
+      };
+  };
 }
