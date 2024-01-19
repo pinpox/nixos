0ba3592b1cb2208c99fb31e23948c59cf7cd1785
Pablo Ovelleiro Corral <mail@pablo.tools>
2024-01-19 13:37:19 +0100

    Set birne deployment ip

--- machines/birne/configuration.nix
+++ machines/birne/configuration.nix
@@ -2,6 +2,8 @@
 { nixpkgs, pkgs, ... }: {
   imports = [ ./hardware-configuration.nix ];
 
+  lollypops.deployment.ssh.host = "192.168.2.84";
+
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
