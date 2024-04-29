4b5d9704df7704f8a82e7b2368452ae1c28a3d21
Pablo Ovelleiro Corral <mail@pablo.tools>
2024-04-29 12:48:41 +0200

    re-enable promterm alias

--- home-manager/modules/shell/zsh.nix
+++ home-manager/modules/shell/zsh.nix
@@ -75,7 +75,7 @@
       cdnh = "cd ~/code/github.com/pinpox/nixos-home";
 
       # Other
-      # pt = "${promterm.defaultPackage.x86_64-linux}/bin/promterm 'https://vpn.prometheus.pablo.tools/api/v1/alerts'";
+      pt = "${promterm.defaultPackage.x86_64-linux}/bin/promterm 'https://vpn.prometheus.pablo.tools/api/v1/alerts'";
       lsblk = "lsblk -o name,mountpoint,label,size,type,uuid";
       c = "${pkgs.bat}/bin/bat -n --decorations never";
       cc = "${pkgs.clang}/bin/clang -Wall -Wextra -pedantic -std=c99 -Wshadow -Weverything";
