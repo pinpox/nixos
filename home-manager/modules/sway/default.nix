48658f6e04e48a5f9799bd5b7302b447fdc356f3
Pablo Ovelleiro Corral <mail@pablo.tools>
2024-04-29 13:13:20 +0200

    sway: include wallpaper

--- home-manager/modules/sway/default.nix
+++ home-manager/modules/sway/default.nix
@@ -1,14 +1,20 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 with lib;
 let
   cfg = config.pinpox.programs.sway;
 
-  start-sway = pkgs.writeShellScriptBin "start-sway" /* sh */
-    ''
-      export WLR_DRM_NO_MODIFIERS=1
-      # dbus-launch --sh-syntax --exit-with-session ${pkgs.sway}/bin/sway
-      ${pkgs.sway}/bin/sway
-    '';
+  start-sway =
+    pkgs.writeShellScriptBin "start-sway" # sh
+      ''
+        export WLR_DRM_NO_MODIFIERS=1
+        # dbus-launch --sh-syntax --exit-with-session ${pkgs.sway}/bin/sway
+        ${pkgs.sway}/bin/sway
+      '';
 in
 {
   options.pinpox.programs.sway.enable = mkEnableOption "sway window manager";
@@ -16,7 +22,6 @@ in
   imports = [ ./swaync/default.nix ];
 
   config = mkIf cfg.enable {
-
 
     # Install these packages for my user
     home.packages = with pkgs; [
@@ -30,32 +35,35 @@ in
       line-awesome
     ];
 
-
     wayland.windowManager.sway = {
       enable = true;
-      config = rec{
-        keybindings = lib.mkOptionDefault
-          {
-            "${modifier}+Return" = "exec ${pkgs.foot}/bin/foot";
-            "${modifier}+p" = "exec ${pkgs.wofi}/bin/wofi --show run";
+      config = rec {
+        keybindings = lib.mkOptionDefault {
+          "${modifier}+Return" = "exec ${pkgs.foot}/bin/foot";
+          "${modifier}+p" = "exec ${pkgs.wofi}/bin/wofi --show run";
 
-            # Cycle in tabbed with win+tab
-            "${modifier}+Shift+Tab" = "focus prev";
-            "${modifier}+Tab" = "focus next";
+          # Cycle in tabbed with win+tab
+          "${modifier}+Shift+Tab" = "focus prev";
+          "${modifier}+Tab" = "focus next";
 
-            # SwayNotificationCenter
-            "${modifier}+n" = "exec swaync-client -t -sw";
+          # SwayNotificationCenter
+          "${modifier}+n" = "exec swaync-client -t -sw";
 
-            "${modifier}+u" = ''[app_id="dropdown"] scratchpad show; [app_id="dropdown"] resize set 98ppt 98ppt; [app_id="dropdown"] move position center'';
-
-          };
+          "${modifier}+u" = ''[app_id="dropdown"] scratchpad show; [app_id="dropdown"] resize set 98ppt 98ppt; [app_id="dropdown"] move position center'';
+        };
 
         modifier = "Mod4"; # Win key
         terminal = "${pkgs.foot}/bin/foot";
 
         startup = [
-          { command = "swaync"; always = true; }
-          { command = "foot --app-id=dropdown"; always = true; }
+          {
+            command = "swaync";
+            always = true;
+          }
+          {
+            command = "foot --app-id=dropdown";
+            always = true;
+          }
           { command = "${pkgs.networkmanagerapplet}/bin/nm-applet --indicator"; }
         ];
 
@@ -66,10 +74,22 @@ in
             criteria.title = "Firefox — Sharing Indicator";
           }
 
-          { command = "floating enable"; criteria.app_id = "dropdown"; }
-          { command = "resize set 98ppt 98ppt"; criteria.app_id = "dropdown"; }
-          { command = "move scratchpad"; criteria.app_id = "dropdown"; }
-          { command = "border pixel 10"; criteria.app_id = "dropdown"; }
+          {
+            command = "floating enable";
+            criteria.app_id = "dropdown";
+          }
+          {
+            command = "resize set 98ppt 98ppt";
+            criteria.app_id = "dropdown";
+          }
+          {
+            command = "move scratchpad";
+            criteria.app_id = "dropdown";
+          }
+          {
+            command = "border pixel 10";
+            criteria.app_id = "dropdown";
+          }
         ];
 
         input = {
@@ -118,7 +138,6 @@ in
               indicator = "#${c.Red}";
               text = "#${c.White}";
             };
-
           };
 
         bars = [
@@ -153,7 +172,7 @@ in
 
         # Set wallpaper for all screens
         # TODO: generated based on coloscheme
-        output."*".bg = "~/Pictures/Wallpaper/nixos-wallpaper.png fill #000000";
+        output."*".bg = "${./nixos-wallpaper.png} fill #000000";
 
         # swaymsg 'output DP-2 mode 2560x1440@165Hz'
 
