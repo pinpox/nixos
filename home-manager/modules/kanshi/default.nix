6cf6de65fe47c08a756924aa8dc8e6caaa6ecac4
Pablo Ovelleiro Corral <mail@pablo.tools>
2024-05-31 11:31:25 +0200

    kanshi: Update syntax

--- home-manager/modules/kanshi/default.nix
+++ home-manager/modules/kanshi/default.nix
@@ -16,41 +16,25 @@ in
     home.packages = with pkgs; [ kanshi ];
 
     # output eDP-1 mode 1920x1080 position 0,0
-    #       output DP-1 mode 2560x1440 position 1080,0
-    #       output DP-2 mode 2560x1440 position 3640,0
+    # output DP-1 mode 2560x1440 position 1080,0
+    # output DP-2 mode 2560x1440 position 3640,0
 
     services.kanshi = {
       enable = true;
-      profiles = {
-
-        laptop-only = {
-          outputs = [
+      settings = [
+        {
+          profile.name = "laptop-only";
+          profile.outputs = [
             {
               criteria = "eDP-1";
               mode = "1920x1080@60Hz";
             }
           ];
-        };
-        triple-home = {
-          outputs = [
-
+        }
+        {
+          profile.name = "triple-home";
+          profile.outputs = [
             # generate with wlay
-            #output "eDP-1" {
-            #mode 1920x1080@60Hz
-            #pos 0 0
-            #transform normal
-            #}
-            #output "DP-1" {
-            #mode 2560x1440@59Hz
-            #pos 4480 0
-            #transform normal
-            #}
-            #output "DP-2" {
-            #mode 2560x1440@59Hz
-            #pos 1920 0
-            #transform normal
-            #}
-
             {
               criteria = "eDP-1";
               mode = "1920x1080@60Hz";
@@ -70,8 +54,8 @@ in
               status = "enable";
             }
           ];
-        };
-      };
+        }
+      ];
     };
   };
 }
