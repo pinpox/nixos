af6189a9a66ca1671c7de76262e2ede1ea03e6b5
Pablo Ovelleiro Corral <mail@pablo.tools>
2025-01-09 17:54:29 +0100

    Set up test-password with password-store

--- flake.nix
+++ flake.nix
@@ -206,7 +206,6 @@
         );
       };
     in
-
     {
 
       devShells = forAllSystems (
---
+++ modules/clan-common/default.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+{
+
+  clan.core.vars.settings.secretStore = "password-store";
+
+  clan.core.vars.generators."mkpasswd-generator" = {
+
+    files.test-password = { };
+    runtimeInputs = with pkgs; [
+      coreutils
+      xkcdpass
+    ];
+    script = ''
+      mkdir -p $out
+      xkcdpass > $out/test-password
+    '';
+  };
+
+  environment.etc."test-password".source =
+    config.clan.core.vars.generators."mkpasswd-generator".files."test-password".path;
+
+}
