11b91e1ceb5b663eb955018b9ef37aaba354e6e2
Pablo Ovelleiro Corral <mail@pablo.tools>
2025-03-04 10:12:10 +0100

    clan-common: add extra caches

--- modules/clan-common/default.nix
+++ modules/clan-common/default.nix
@@ -29,4 +29,13 @@
   environment.etc."test-password".source =
     config.clan.core.vars.generators."mkpasswd-generator".files."test-password".path;
 
+  nix.settings.trusted-substituters = [
+    "https://cache.clan.lol"
+    "https://nix-community.cachix.org"
+  ];
+  nix.settings.trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "cache.clan.lol-1:3KztgSAB5R1M+Dz7vzkBGzXdodizbgLXGXKXlcQLA28="
+  ];
+
 }
