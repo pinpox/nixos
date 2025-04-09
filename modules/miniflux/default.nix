26d78bb76f550b048995c2ba0f7796fd81891cff
pinpox <github@pablo.tools>
2025-04-09 11:52:33 +0200

    modules/miniflux: clanify

--- modules/miniflux/default.nix
+++ modules/miniflux/default.nix
@@ -1,7 +1,7 @@
 {
   config,
-  pkgs,
   lib,
+  pkgs,
   ...
 }:
 with lib;
@@ -16,10 +16,23 @@ in
 
   config = mkIf cfg.enable {
 
-    lollypops.secrets.files = {
-      "miniflux/credentials" = { };
-      "miniflux/oauth2_client_id_file" = { };
-      "miniflux/oauth2_client_secret_file" = { };
+    clan.core.vars.generators."miniflux" = {
+      files.credentials = { };
+
+      # From Gitea
+      prompts.oauth2_client_id.persist = true;
+      prompts.oauth2_client_secret.persist = true;
+
+      runtimeInputs = with pkgs; [
+        coreutils
+        xkcdpass
+      ];
+
+      script = # sh
+        ''
+          mkdir -p $out
+          printf "ADMIN_USERNAME=admin\nADMIN_PASSWORD='%s'" "$(xkcdpass -d-)" > $out/credentials
+        '';
     };
 
     services.caddy = {
@@ -28,39 +41,25 @@ in
         "reverse_proxy ${config.services.miniflux.config.LISTEN_ADDR}";
     };
 
-    systemd.services.miniflux = {
-      serviceConfig = {
-        LoadCredential = [
-          "oauth2_client_id_file:${config.lollypops.secrets.files."miniflux/oauth2_client_id_file".path}"
-          "oauth2_client_secret_file:${
-            config.lollypops.secrets.files."miniflux/oauth2_client_secret_file".path
-          }"
-        ];
-      };
-    };
+    systemd.services.miniflux.serviceConfig.LoadCredential =
+      with config.clan.core.vars.generators."miniflux".files; [
+        "oauth2_client_id_file:${oauth2_client_id.path}"
+        "oauth2_client_secret_file:${oauth2_client_secret.path}"
+      ];
 
     services.miniflux = {
       enable = true;
       config = {
+        # OAUTH2_USER_CREATION = "1";
         CLEANUP_FREQUENCY = "48";
         LISTEN_ADDR = "127.0.0.1:8787";
         OAUTH2_PROVIDER = "oidc";
-
-        # See: https://www.freedesktop.org/software/systemd/man/systemd.exec.html
-        # In order to reference the path a credential may be read from within a
-        # ExecStart= command line use "${CREDENTIALS_DIRECTORY}/mycred", e.g.
-        # "ExecStart=cat ${CREDENTIALS_DIRECTORY}/mycred". In order to reference the
-        # path a credential may be read from within a Environment= line use
-        # "%d/mycred", e.g. "Environment=MYCREDPATH=%d/mycred".
-
         OAUTH2_CLIENT_ID_FILE = "%d/oauth2_client_id_file";
         OAUTH2_CLIENT_SECRET_FILE = "%d/oauth2_client_secret_file";
-
         OAUTH2_REDIRECT_URL = "https://news.0cx.de/oauth2/oidc/callback";
-        # OAUTH2_USER_CREATION = "1";
         OAUTH2_OIDC_DISCOVERY_ENDPOINT = "https://git.0cx.de/";
       };
-      adminCredentialsFile = config.lollypops.secrets.files."miniflux/credentials".path;
+      adminCredentialsFile = config.clan.core.vars.generators."miniflux".files."credentials".path;
     };
   };
 }
