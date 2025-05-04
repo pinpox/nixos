19073a9da7a8708cd8bb99143f3b5faae8983e3c
pinpox <git@pablo.tools>
2025-05-04 13:48:05 +0200

    home-manager/mako: fix deprecated options

--- home-manager/modules/mako/default.nix
+++ home-manager/modules/mako/default.nix
@@ -18,29 +18,32 @@ in
 
     services.mako = {
       enable = true;
-      anchor = "top-right";
-      backgroundColor = "#285577FF";
-      borderColor = "#4C7899FF";
-      # progressColor = "over #5588AAFF";
-      textColor = "#FFFFFFFF";
-      borderRadius = 5;
-      borderSize = 5;
-      defaultTimeout = 10000; # In milliseconds
-      # extraConfig = '''';
-      font = "Berkeley Mono 12";
-      # %a   Application name
-      # %s   Notification summary
-      # %b   Notification body
-      # %g   Number of notifications in the current group
-      # %i   Notification id
-      # format = "<b>%s</b>\\n%b";
-      # groupBy = "";
-      height = 200;
-      width = 300;
-      # iconPath = "";
-      icons = true;
-      margin = "10";
-      padding = "5";
+
+      settings = {
+        anchor = "top-right";
+        backgroundColor = "#285577FF";
+        borderColor = "#4C7899FF";
+        # progressColor = "over #5588AAFF";
+        textColor = "#FFFFFFFF";
+        borderRadius = "5";
+        borderSize = "5";
+        defaultTimeout = "10000"; # In milliseconds
+        # extraConfig = '''';
+        font = "Berkeley Mono 12";
+        # %a   Application name
+        # %s   Notification summary
+        # %b   Notification body
+        # %g   Number of notifications in the current group
+        # %i   Notification id
+        # format = "<b>%s</b>\\n%b";
+        # groupBy = "";
+        height = "200";
+        width = "300";
+        # iconPath = "";
+        icons = "true";
+        margin = "10";
+        padding = "5";
+      };
     };
   };
 }
