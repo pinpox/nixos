6e41d38e997a37bc5e8a6445cdd244e7e986d7dd
pinpox <git@pablo.tools>
2025-09-01 09:53:20 +0200

    machines/limette: set to btrfs config

--- machines/limette/configuration.nix
+++ machines/limette/configuration.nix
@@ -8,17 +8,15 @@
   networking.hostName = "limette";
 
   pinpox.defaults.lvm-grub.enable = true;
-
   boot.growPartition = true;
 
   hardware.enableRedistributableFirmware = true;
   imports = [
     nixos-hardware.nixosModules.lenovo-thinkpad-x230
-    ./disko-config-zfs.nix
+    ./disko-config-btrfs.nix
   ];
 
   disko.imageBuilder.extraDependencies = [ pkgs.kmod ];
-
 
   hardware.graphics.extraPackages = [
     pkgs.intel-media-driver # LIBVA_DRIVER_NAME=iHD
