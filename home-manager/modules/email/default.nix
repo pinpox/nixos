b5c9837c6dd3cec0b9debadadb78cfd1c9a01194
pinpox <git@pablo.tools>
2026-01-12 12:42:20 +0100

    home-manager: extract email module

---
+++ home-manager/modules/email/default.nix
@@ -0,0 +1,96 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with lib;
+let
+  cfg = config.pinpox.defaults.email;
+in
+{
+  options.pinpox.defaults.email.enable = mkEnableOption "email client config";
+
+  config = mkIf cfg.enable {
+
+    accounts.email.maildirBasePath = "Mail";
+
+    programs.aerc = {
+      extraConfig.general.unsafe-accounts-conf = true;
+      enable = true;
+      # stylesets.pinpox = (import ./aerc-style.nix { inherit config;});
+
+      extraConfig = {
+        ui = {
+          # styleset-name = "pinpox";
+          icon-attachment = "a ";
+          icon-old = "";
+          icon-replied = "↩ ";
+        };
+        compose.address-book-cmd = "carddav-query %s";
+
+        filters = {
+          "text/plain" = "colorize";
+          "text/calendar" = "calendar";
+          "message/delivery-status" = "colorize";
+          "message/rfc822" = "colorize";
+          #text/html=pandoc -f html -t plain | colorize
+          "text/html" = "! html";
+          #text/html=! w3m -T text/html -I UTF-8
+          #text/*=bat -fP --file-name="$AERC_FILENAME"
+          #application/x-sh=bat -fP -l sh
+          #image/*=catimg -w $(tput cols) -
+          #subject,~Git(hub|lab)=lolcat -f
+          #from,thatguywhodoesnothardwraphismessages=wrap -w 100 | colorize
+          ".headers" = "colorize";
+          "image/*" = "${pkgs.libsixel}/bin/img2sixel - ";
+          # image/*=catimg -w$(tput cols) -
+        };
+      };
+    };
+
+    accounts.email.accounts = {
+      pablo_tools = {
+        folders = {
+          # send = "SENT";
+          inbox = "INBOX";
+        };
+        aerc.enable = true;
+        address = "mail@pablo.tools";
+
+        aliases = [
+          "git@pablo.tools"
+          "github@pablo.tools"
+          "pablo1@mailbox.org"
+        ];
+
+        realName = "Pablo Ovelleiro Corral";
+        primary = true;
+        maildir.path = "pablo_tools";
+
+        signature = {
+          text = ''
+            Pablo Ovelleiro Corral
+
+            Web:     https://pablo.tools
+            Matrix:  @pinpox:matrix.org
+            Github:  https://github.com/pinpox
+          '';
+          showSignature = "append";
+        };
+
+        userName = "pablo1@mailbox.org";
+        passwordCommand = "passage show mailbox.org/himalaya";
+        imap = {
+          host = "imap.mailbox.org";
+          tls.enable = true;
+        };
+        smtp = {
+          host = "smtp.mailbox.org";
+          port = 465;
+        };
+      };
+    };
+
+  };
+}
