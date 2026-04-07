fbf945ff72a5764cc769331ee6b78a6b5b6b6537
pinpox <git@pablo.tools>
2026-04-07 13:45:43 +0200

    modules/monitoring: delete promtail (deprecated)

--- modules/monitoring/loki.nix
+++ modules/monitoring/loki.nix
@@ -91,39 +91,5 @@ in
         compactor.working_directory = "/var/lib/loki/boltdb-shipper-compactor";
       };
     };
-
-    services.promtail = {
-      enable = true;
-      configuration = {
-
-        server = {
-          http_listen_port = 28183;
-          grpc_listen_port = 0;
-        };
-
-        positions.filename = "/tmp/positions.yml";
-
-        clients = [ { url = "http://localhost:${toString port-loki}/loki/api/v1/push"; } ];
-
-        scrape_configs = [
-          {
-            job_name = "journal";
-            journal = {
-              max_age = "12h";
-              labels = {
-                job = "systemd-journal";
-                host = "${config.networking.hostName}";
-              };
-            };
-            relabel_configs = [
-              {
-                source_labels = [ "__journal__systemd_unit" ];
-                target_label = "unit";
-              }
-            ];
-          }
-        ];
-      };
-    };
   };
 }
