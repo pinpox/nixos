66180411b09b81430b122c383a6f1cf5914569ae
pinpox <git@pablo.tools>
2026-05-28 22:52:42 +0200

    home-manager/ssh: fix deprecated config options

--- home-manager/modules/ssh/default.nix
+++ home-manager/modules/ssh/default.nix
@@ -21,45 +21,37 @@ in
       settings = {
 
         "*" = {
-          certificateFile = [ "${./yubikey-ssh-cert.pub}" ];
-          extraOptions = {
-            ForwardAgent = "no";
-            ServerAliveInterval = "0";
-            ServerAliveCountMax = "3";
-            Compression = "no";
-            AddKeysToAgent = "yes";
-            HashKnownHosts = "no";
-            UserKnownHostsFile = "~/.ssh/known_hosts";
-            ControlMaster = "auto";
-            ControlPath = "~/.ssh/master-%r@%n:%p";
-            ControlPersist = "1h";
-            PKCS11Provider = "/run/current-system/sw/lib/opensc-pkcs11.so";
-          };
+          CertificateFile = [ "${./yubikey-ssh-cert.pub}" ];
+          ForwardAgent = "no";
+          ServerAliveInterval = 0;
+          ServerAliveCountMax = 3;
+          Compression = "no";
+          AddKeysToAgent = "yes";
+          HashKnownHosts = "no";
+          UserKnownHostsFile = "~/.ssh/known_hosts";
+          ControlMaster = "auto";
+          ControlPath = "~/.ssh/master-%r@%n:%p";
+          ControlPersist = "1h";
+          PKCS11Provider = "/run/current-system/sw/lib/opensc-pkcs11.so";
         };
 
         "ap-oben" = {
-          hostname = "UAP-ProOBEN.lan";
-          user = "pinpox";
-          extraOptions = {
-            PubkeyAcceptedAlgorithms = "+ssh-rsa";
-            HostkeyAlgorithms = "+ssh-rsa";
-          };
+          HostName = "UAP-ProOBEN.lan";
+          User = "pinpox";
+          PubkeyAcceptedAlgorithms = "+ssh-rsa";
+          HostkeyAlgorithms = "+ssh-rsa";
         };
         "ap-mitte" = {
-          hostname = "UAP-ProMITTE.lan";
-          user = "pinpox";
-          extraOptions = {
-            PubkeyAcceptedAlgorithms = "+ssh-rsa";
-            HostkeyAlgorithms = "+ssh-rsa";
-          };
+          HostName = "UAP-ProMITTE.lan";
+          User = "pinpox";
+          PubkeyAcceptedAlgorithms = "+ssh-rsa";
+          HostkeyAlgorithms = "+ssh-rsa";
         };
         "ap-unten" = {
-          hostname = "UAP-ProUNTEN.lan";
-          user = "pinpox";
-          extraOptions = {
-            PubkeyAcceptedAlgorithms = "+ssh-rsa";
-            HostkeyAlgorithms = "+ssh-rsa";
-          };
+          HostName = "UAP-ProUNTEN.lan";
+          User = "pinpox";
+          PubkeyAcceptedAlgorithms = "+ssh-rsa";
+          HostkeyAlgorithms = "+ssh-rsa";
         };
       };
     };
